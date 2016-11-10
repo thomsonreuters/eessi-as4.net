7e842973ff794c67215cad350a7de9c4d04cadbb
Stijn Moreels <stijn.moreels@codit.eu>
2016-11-10 11:03:28 +0100

    Unit test Factory + Serializer

---
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Security/Factories/GivenEncodingFactoryFacts.cs
@@ -0,0 +1,35 @@
+﻿using Eu.EDelivery.AS4.Security.Factories;
+using Org.BouncyCastle.Crypto.Encodings;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.UnitTests.Security.Factories
+{
+    /// <summary>
+    /// Testing <see cref="EncodingFactory"/>
+    /// </summary>
+    public class GivenEncodingFactoryFacts
+    {
+        public class GivenValidArguments : GivenEncodingFactoryFacts
+        {
+            [Fact]
+            public void ThenCreateEncodingWithDefaultsSucceeds()
+            {
+                // Act
+                OaepEncoding encoding = EncodingFactory.Instance.Create();
+                // Assert
+                Assert.Equal("RSA/OAEPPadding", encoding.AlgorithmName);
+            }
+
+            [Fact]
+            public void ThenCreateEncodingWithGivenAlgorithmSucceeds()
+            {
+                // Arrange
+                const string algorithmName = "http://www.w3.org/2001/04/xmlenc#sha256";
+                // Act
+                OaepEncoding encoding = EncodingFactory.Instance.Create(algorithmName);
+                // Assert
+                Assert.Equal("RSA/OAEPPadding", encoding.AlgorithmName);
+            }
+        }
+    }
+}
