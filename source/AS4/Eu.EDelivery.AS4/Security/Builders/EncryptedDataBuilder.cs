132611e5efc04c0ccb9f7546773ae271f96f63c0
Stijn Moreels <stijn.moreels@codit.eu>
2016-11-10 10:02:10 +0100

    Move builders

---
+++ source/AS4/Eu.EDelivery.AS4/Security/Builders/EncryptedDataBuilder.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Security.Cryptography.Xml;
+using Eu.EDelivery.AS4.Security.Encryption;
+using Eu.EDelivery.AS4.Security.References;
+using Eu.EDelivery.AS4.Security.Transforms;
+
+namespace Eu.EDelivery.AS4.Security.Builders
+{
+    /// <summary>
+    /// Builder to create <see cref="EncryptedData"/> Models
+    /// </summary>
+    internal class EncryptedDataBuilder
+    {
+        private DataEncryptionConfiguration _data;
+        private string _mimeType;
+        private string _uri;
+        private string _referenceId;
+
+        /// <summary>
+        /// Add a <paramref name="referenceId"/> to the <see cref="EncryptedData"/>
+        /// </summary>
+        /// <param name="referenceId"></param>
+        /// <returns></returns>
+        public EncryptedDataBuilder WithReferenceId(string referenceId)
+        {
+            this._referenceId = referenceId;
+            return this;
+        }
+
+        /// <summary>
+        /// Add a <paramref name="uri"/> to the <see cref="EncryptedData"/>
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public EncryptedDataBuilder WithUri(string uri)
+        {
+            this._uri = uri;
+            return this;
+        }
+
+        /// <summary>
+        /// Add a <paramref name="mimeType"/> to the <see cref="EncryptedData"/>
+        /// </summary>
+        /// <param name="mimeType"></param>
+        /// <returns></returns>
+        public EncryptedDataBuilder WithMimeType(string mimeType)
+        {
+            this._mimeType = mimeType;
+            return this;
+        }
+
+        /// <summary>
+        /// Add a <paramref name="data"/> to the <see cref="EncryptedData"/>
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public EncryptedDataBuilder WithDataEncryptionConfiguration(DataEncryptionConfiguration data)
+        {
+            this._data = data;
+            return this;
+        }
+
+        /// <summary>
+        /// Build the <see cref="EncryptedData"/> Model
+        /// </summary>
+        /// <returns></returns>
+        public EncryptedData Build()
+        {
+            EncryptedData encryptedData = CreateEncryptedData();
+            AssemblyEncryptedData(encryptedData);
+
+            return encryptedData;
+        }
+
+        private EncryptedData CreateEncryptedData()
+        {
+            return new EncryptedData
+            {
+                Id = "ed-" + Guid.NewGuid(),
+                Type = this._data.EncryptionType,
+                EncryptionMethod = new EncryptionMethod(this._data.EncryptionMethod),
+                CipherData = new CipherData(),
+                MimeType = this._mimeType
+            };
+        }
+
+        private void AssemblyEncryptedData(EncryptedData encryptedData)
+        {
+            encryptedData.CipherData.CipherReference = new CipherReference("cid:" + this._uri);
+            encryptedData.CipherData.CipherReference.TransformChain.Add(new AttachmentCiphertextTransform());
+            encryptedData.KeyInfo.AddClause(new ReferenceSecurityTokenReference {ReferenceId = this._referenceId});
+        }
+    }
+}
---
+++ source/AS4/Eu.EDelivery.AS4/Security/Factories/EncodingFactory.cs
@@ -0,0 +1,45 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Security;
+
+namespace Eu.EDelivery.AS4.Security.Factories
+{
+    /// <summary>
+    /// Factory to create <see cref="OaepEncoding"/> Encoding
+    /// </summary>
+    internal class EncodingFactory
+    {
+        private static readonly EncodingFactory Signalton = new EncodingFactory();
+
+        public static EncodingFactory Instance = Signalton;
+
+        private EncodingFactory() {}
+
+        /// <summary>
+        /// Create and <see cref="OaepEncoding"/> instance
+        /// </summary>
+        /// <param name="digestAlgorithm"></param>
+        /// <returns></returns>
+        public OaepEncoding Create(string digestAlgorithm = null)
+        {
+            IDigest digest = digestAlgorithm != null ? GetDigest(digestAlgorithm) : new Sha256Digest();
+            return CreateEncoding(digest);
+        }
+
+        private IDigest GetDigest(string algorithm)
+        {
+            return DigestUtilities.GetDigest(algorithm.Substring(algorithm.IndexOf('#') + 1));
+        }
+
+        private OaepEncoding CreateEncoding(IDigest digest)
+        {
+            return new OaepEncoding(
+                cipher: new RsaEngine(),
+                hash: digest,
+                mgf1Hash: new Sha1Digest(),
+                encodingParams: new byte[0]);
+        }
+    }
+}
