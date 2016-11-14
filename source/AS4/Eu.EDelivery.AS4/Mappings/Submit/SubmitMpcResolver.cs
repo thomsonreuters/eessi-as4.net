c2f3608b30a29fed19c870acaf142009eced0762
Stijn Moreels <stijn.moreels@codit.eu>
2016-11-14 08:05:21 +0100

    Cleanup submit resolvers

---
+++ source/AS4/Eu.EDelivery.AS4/Mappings/Submit/SubmitMpcResolver.cs
@@ -0,0 +1,37 @@
+﻿using Eu.EDelivery.AS4.Exceptions;
+using Eu.EDelivery.AS4.Model.Submit;
+
+namespace Eu.EDelivery.AS4.Mappings.Submit
+{
+    /// <summary>
+    /// 1. SubmitMessage / MessageInfo / Mpc
+    /// 2. PMode / Message Packaging / Mpc
+    /// 3. No mpc attribute
+    /// </summary>
+    public class SubmitMpcResolver : ISubmitResolver<string>
+    {
+        /// <summary>
+        /// Resolve the Mpc from the <paramref name="submitMessage"/>
+        /// </summary>
+        /// <param name="submitMessage"></param>
+        /// <returns></returns>
+        public string Resolve(SubmitMessage submitMessage)
+        {
+            if (DoesSubmitMessageTriesToOverridePModeMpc(submitMessage))
+                throw new AS4Exception($"Submit Message is not allowed by PMode {submitMessage.PMode.Id} to override Mpc");
+
+            if (submitMessage.PMode.AllowOverride && submitMessage.MessageInfo.Mpc != null)
+                return submitMessage.MessageInfo.Mpc;
+
+            return submitMessage.PMode.MessagePackaging.Mpc;
+        }
+
+        private bool DoesSubmitMessageTriesToOverridePModeMpc(SubmitMessage submitMessage)
+        {
+            return
+                submitMessage.PMode.AllowOverride == false &&
+                !string.IsNullOrEmpty(submitMessage.MessageInfo.Mpc) &&
+                !string.IsNullOrEmpty(submitMessage.PMode.MessagePackaging.Mpc);
+        }
+    }
+}
