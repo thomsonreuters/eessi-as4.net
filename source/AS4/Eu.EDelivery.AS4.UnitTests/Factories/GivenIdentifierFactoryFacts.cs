d4f70ade46aca25b97e20bfbf16687cd823a370a
Stijn Moreels <stijn.moreels@codit.eu>
2016-11-16 15:44:08 +0100

    Add missing unit tests

---
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Factories/GivenIdentifierFactoryFacts.cs
@@ -0,0 +1,32 @@
+﻿using Eu.EDelivery.AS4.Factories;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.UnitTests.Factories
+{
+    /// <summary>
+    /// Testing <seealso cref="IdentifierFactory" />
+    /// </summary>
+    public class GivenIdentifierFactoryFacts
+    {
+        [Fact]
+        public void ThenGenerateIdGuidAndIpAddressCorrectIdGenerated()
+        {
+            // Act
+            string id = IdentifierFactory.Instance.Create("{GUID}@{IPADDRESS}");
+            // Assert
+            string[] splittedId = id.Split('@');
+            Assert.Matches(@"\w+-\w+-\w+-\w+-\w+", splittedId[0]);
+            Assert.Matches(@"\d+\.\d+\.\d+\.\d+", splittedId[1]);
+        }
+
+        [Fact]
+        public void ThenGenerateIdMachineNameCorrectIdGenerated()
+        {
+            // Act
+            string id = IdentifierFactory.Instance.Create("{MACHINENAME}");
+            // Assert
+            Assert.NotEqual("{MACHINENAME}", id);
+            Assert.Matches(@"\w+", id);
+        }
+    }
+}
---
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Mappings/Notify/GivenReceiptToNotifyMapFacts.cs
@@ -0,0 +1,50 @@
+﻿using Eu.EDelivery.AS4.Factories;
+using Eu.EDelivery.AS4.Mappings.Common;
+using Eu.EDelivery.AS4.Mappings.Notify;
+using Eu.EDelivery.AS4.Model.Core;
+using Eu.EDelivery.AS4.Model.Notify;
+using Eu.EDelivery.AS4.Singletons;
+using Eu.EDelivery.AS4.UnitTests.Common;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.UnitTests.Mappings.Notify
+{
+    /// <summary>
+    /// Testing <see cref="ReceiptToNotifyMap"/>
+    /// </summary>
+    public class GivenReceiptToNotifyMapFacts
+    {
+        public GivenReceiptToNotifyMapFacts()
+        {
+            MapInitialization.InitializeMapper();
+            IdentifierFactory.Instance.SetContext(StubConfig.Instance);
+        }
+
+        public class GivenValidArguments : GivenReceiptToNotifyMapFacts
+        {
+            [Fact]
+            public void ThenMapMessageInfoSucceeds()
+            {
+                // Arrange
+                var receipt = new Receipt("message-id") {RefToMessageId = "ref-to-message-id"};
+                // Act
+                var notifyMessage = AS4Mapper.Map<NotifyMessage>(receipt);
+                // Assert
+                MessageInfo notifyMessageInfo = notifyMessage.MessageInfo;
+                Assert.Equal(receipt.MessageId, notifyMessageInfo.MessageId);
+                Assert.Equal(receipt.RefToMessageId, notifyMessageInfo.RefToMessageId);
+            }
+
+            [Fact]
+            public void ThenNotifyMessageHasStatusDelivered()
+            {
+                // Arrange
+                var receipt = new Receipt("message-id");
+                // Act
+                var notifyMessage = AS4Mapper.Map<NotifyMessage>(receipt);
+                // Assert
+                Assert.Equal(Status.Delivered, notifyMessage.StatusInfo.Status);
+            }
+        }
+    }
+}
