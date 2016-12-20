56f38ca14c4ec139a7bb9c3b98dfd0bcdfac7f15
jtilburgh <jan.tilburgh@codit.eu>
2016-12-20 11:59:42 +0100

    Move to automapper 5.0.2 because the AS4.net lib is currently incompatible with a higher version.

--- source/Fe/src/Eu.EDelivery.AS4.Fe/ui/app/api/ReceiveReceiptHandling.ts
+++ source/Fe/src/Eu.EDelivery.AS4.Fe/ui/app/api/ReceiveReceiptHandling.ts
@@ -1,5 +1,6 @@
 /* tslint:disable */
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 export class ReceiveReceiptHandling {
 	useNNRFormat: boolean;
@@ -7,29 +8,28 @@ export class ReceiveReceiptHandling {
 	callbackUrl: string;
 	sendingPMode: string;
 
-	static FIELD_useNNRFormat: string = 'useNNRFormat';	
-	static FIELD_replyPattern: string = 'replyPattern';	
-	static FIELD_callbackUrl: string = 'callbackUrl';	
-	static FIELD_sendingPMode: string = 'sendingPMode';	
+	static FIELD_useNNRFormat: string = 'useNNRFormat';
+	static FIELD_replyPattern: string = 'replyPattern';
+	static FIELD_callbackUrl: string = 'callbackUrl';
+	static FIELD_sendingPMode: string = 'sendingPMode';
 
 	static getForm(formBuilder: FormBuilder, current: ReceiveReceiptHandling): FormGroup {
 		return formBuilder.group({
-			useNNRFormat: [!!(current && current.useNNRFormat)],
-			replyPattern: [current && current.replyPattern],
-			callbackUrl: [current && current.callbackUrl],
-			sendingPMode: [current && current.sendingPMode],
+			[this.FIELD_useNNRFormat]: [!!(current && current.useNNRFormat), Validators.required],
+			[this.FIELD_replyPattern]: [current && current.replyPattern, Validators.required],
+			[this.FIELD_callbackUrl]: [current && current.callbackUrl],
+			[this.FIELD_sendingPMode]: [current && current.sendingPMode],
 		});
 	}
 	/// Patch up all the formArray controls
 	static patchForm(formBuilder: FormBuilder, form: FormGroup, current: ReceiveReceiptHandling) {
-		form.removeControl('useNNRFormat');
-		form.addControl('useNNRFormat', formBuilder.control(current && current.useNNRFormat));
-		form.removeControl('replyPattern');
-		form.addControl('replyPattern', formBuilder.control(current && current.replyPattern));
-		form.removeControl('callbackUrl');
-		form.addControl('callbackUrl', formBuilder.control(current && current.callbackUrl));
-		form.removeControl('sendingPMode');
-		form.addControl('sendingPMode', formBuilder.control(current && current.sendingPMode));
-
+		form.removeControl(this.FIELD_useNNRFormat);
+		form.addControl(this.FIELD_useNNRFormat, formBuilder.control(current && current.useNNRFormat, Validators.required));
+		form.removeControl(this.FIELD_replyPattern);
+		form.addControl(this.FIELD_replyPattern, formBuilder.control(current && current.replyPattern, Validators.required));
+		form.removeControl(this.FIELD_callbackUrl);
+		form.addControl(this.FIELD_callbackUrl, formBuilder.control(current && current.callbackUrl));
+		form.removeControl(this.FIELD_sendingPMode);
+		form.addControl(this.FIELD_sendingPMode, formBuilder.control(current && current.sendingPMode));
 	}
 }
--- source/Fe/src/Eu.EDelivery.AS4.Fe/ui/app/api/ReceivingProcessingMode.ts
+++ source/Fe/src/Eu.EDelivery.AS4.Fe/ui/app/api/ReceivingProcessingMode.ts
@@ -1,5 +1,5 @@
 /* tslint:disable */
-import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { ReceiveReliability } from "./ReceiveReliability";
 import { ReceiveReceiptHandling } from "./ReceiveReceiptHandling";
 import { ReceiveErrorHandling } from "./ReceiveErrorHandling";
@@ -34,39 +34,39 @@ export class ReceivingProcessingMode {
 
     static getForm(formBuilder: FormBuilder, current: ReceivingProcessingMode): FormGroup {
         return formBuilder.group({
-            id: [current && current.id],
-            mep: [current && current.mep],
-            mepBinding: [current && current.mepBinding],
-            reliability: ReceiveReliability.getForm(formBuilder, current && current.reliability),
-            receiptHandling: ReceiveReceiptHandling.getForm(formBuilder, current && current.receiptHandling),
-            errorHandling: ReceiveErrorHandling.getForm(formBuilder, current && current.errorHandling),
-            exceptionHandling: Receivehandling.getForm(formBuilder, current && current.exceptionHandling),
-            security: ReceiveSecurity.getForm(formBuilder, current && current.security),
-            messagePackaging: MessagePackaging.getForm(formBuilder, current && current.messagePackaging),
-            deliver: Deliver.getForm(formBuilder, current && current.deliver),
+            [this.FIELD_id]: [current && current.id, Validators.required],
+            [this.FIELD_mep]: [current && current.mep, Validators.required],
+            [this.FIELD_mepBinding]: [current && current.mepBinding, Validators.required],
+            [this.FIELD_reliability]: ReceiveReliability.getForm(formBuilder, current && current.reliability),
+            [this.FIELD_receiptHandling]: ReceiveReceiptHandling.getForm(formBuilder, current && current.receiptHandling),
+            [this.FIELD_errorHandling]: ReceiveErrorHandling.getForm(formBuilder, current && current.errorHandling),
+            [this.FIELD_exceptionHandling]: Receivehandling.getForm(formBuilder, current && current.exceptionHandling),
+            [this.FIELD_security]: ReceiveSecurity.getForm(formBuilder, current && current.security),
+            [this.FIELD_messagePackaging]: MessagePackaging.getForm(formBuilder, current && current.messagePackaging),
+            [this.FIELD_deliver]: Deliver.getForm(formBuilder, current && current.deliver),
         });
     }
     /// Patch up all the formArray controls
     static patchForm(formBuilder: FormBuilder, form: FormGroup, current: ReceivingProcessingMode) {
-        form.setControl('id', formBuilder.control({ value: current && current.id, disabled: !!!current }));
-        form.removeControl('mep');
-        form.addControl('mep', formBuilder.control(current && current.mep));
-        form.removeControl('mepBinding');
-        form.addControl('mepBinding', formBuilder.control(current && current.mepBinding));
+        form.setControl(this.FIELD_id, formBuilder.control({ value: current && current.id, disabled: !!!current }));
+        form.removeControl(this.FIELD_mep);
+        form.addControl(this.FIELD_mep, formBuilder.control(current && current.mep));
+        form.removeControl(this.FIELD_mepBinding);
+        form.addControl(this.FIELD_mepBinding, formBuilder.control(current && current.mepBinding));
 
-        form.removeControl('reliability');
-        form.addControl('reliability', ReceiveReliability.getForm(formBuilder, current && current.reliability));
-        form.removeControl('receiptHandling');
-        form.addControl('receiptHandling', ReceiveReceiptHandling.getForm(formBuilder, current && current.receiptHandling));
-        form.removeControl('errorHandling');
-        form.addControl('errorHandling', ReceiveErrorHandling.getForm(formBuilder, current && current.errorHandling));
-        form.removeControl('exceptionHandling');
-        form.addControl('exceptionHandling', Receivehandling.getForm(formBuilder, current && current.exceptionHandling));
-        form.removeControl('security');
-        form.addControl('security', ReceiveSecurity.getForm(formBuilder, current && current.security));
-        form.removeControl('messagePackaging');
-        form.addControl('messagePackaging', MessagePackaging.getForm(formBuilder, current && current.messagePackaging));
-        form.removeControl('deliver');
-        form.addControl('deliver', Deliver.getForm(formBuilder, current && current.deliver));
+        form.removeControl(this.FIELD_reliability);
+        form.addControl(this.FIELD_reliability, ReceiveReliability.getForm(formBuilder, current && current.reliability));
+        form.removeControl(this.FIELD_receiptHandling);
+        form.addControl(this.FIELD_receiptHandling, ReceiveReceiptHandling.getForm(formBuilder, current && current.receiptHandling));
+        form.removeControl(this.FIELD_errorHandling);
+        form.addControl(this.FIELD_errorHandling, ReceiveErrorHandling.getForm(formBuilder, current && current.errorHandling));
+        form.removeControl(this.FIELD_exceptionHandling);
+        form.addControl(this.FIELD_exceptionHandling, Receivehandling.getForm(formBuilder, current && current.exceptionHandling));
+        form.removeControl(this.FIELD_security);
+        form.addControl(this.FIELD_security, ReceiveSecurity.getForm(formBuilder, current && current.security));
+        form.removeControl(this.FIELD_messagePackaging);
+        form.addControl(this.FIELD_messagePackaging, MessagePackaging.getForm(formBuilder, current && current.messagePackaging));
+        form.removeControl(this.FIELD_deliver);
+        form.addControl(this.FIELD_deliver, Deliver.getForm(formBuilder, current && current.deliver));
     }
 }
