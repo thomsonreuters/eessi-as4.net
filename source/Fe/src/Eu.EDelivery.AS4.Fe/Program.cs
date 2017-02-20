fa46fa0b25ce251ec9d74bcf76fd868ab529f755
jtilburgh <jan.tilburgh@codit.eu>
2017-02-20 18:53:57 +0100

    Make the kestrel port configurable.

--- source/Fe/src/Eu.EDelivery.AS4.Fe/Program.cs
+++ source/Fe/src/Eu.EDelivery.AS4.Fe/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Eu.EDelivery.AS4.Fe
 {
@@ -8,10 +9,16 @@ namespace Eu.EDelivery.AS4.Fe
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(args != null && args.Contains("inprocess") ? "appsettings.inprocess.json" : "appsettings.json", optional: true)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseEnvironment(args != null && args.Contains("inprocess") ? "inprocess" : "production")
                 .UseKestrel()
                 .UseWebRoot("ui/dist")
+                .UseUrls(config["Port"] ?? "http://localhost:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
