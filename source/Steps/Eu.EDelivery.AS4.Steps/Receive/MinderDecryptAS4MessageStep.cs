5a4298da175fea334c59951a579d0ec7d3bda447
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-02-21 10:49:06 +0000

    Refactoring DecryptStep

--- source/Steps/Eu.EDelivery.AS4.Steps/Receive/MinderDecryptAS4MessageStep.cs
+++ source/Steps/Eu.EDelivery.AS4.Steps/Receive/MinderDecryptAS4MessageStep.cs
@@ -9,15 +9,6 @@ namespace Eu.EDelivery.AS4.Steps.Receive
 {
     public class MinderDecryptAS4MessageStep : DecryptAS4MessageStep
     {
-        protected override Tuple<X509FindType, string> GetCertificateFindValue(InternalMessage internalMessage)
-        {
-            Decryption decryption = internalMessage.AS4Message.ReceivingPMode.Security.Decryption;
-
-            MessageProperty property = internalMessage.AS4Message.PrimaryUserMessage?.MessageProperties.FirstOrDefault(m => m.Name.Equals("originalSender", StringComparison.OrdinalIgnoreCase));
-
-            string findValue = property?.Value.Equals("C1", StringComparison.OrdinalIgnoreCase) == true ? "as4-net-c3" : "as4-net-c2";
-
-            return new Tuple<X509FindType, string>(decryption.PrivateKeyFindType, findValue);
-        }
+        
     }
 }
