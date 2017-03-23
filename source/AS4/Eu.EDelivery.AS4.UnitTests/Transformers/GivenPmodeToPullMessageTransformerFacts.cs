9d479bdbd032fb05c525f4f7a77155db2cbf9552
Stijn Moreels <stijn.moreels@codit.eu>
2017-03-23 10:40:13 +0100

    Create 'Pull Request Transformer'

---
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Transformers/GivenPmodeToPullMessageTransformerFacts.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Eu.EDelivery.AS4.Factories;
+using Eu.EDelivery.AS4.Model.Core;
+using Eu.EDelivery.AS4.Model.Internal;
+using Eu.EDelivery.AS4.Model.PMode;
+using Eu.EDelivery.AS4.Transformers;
+using Eu.EDelivery.AS4.UnitTests.Common;
+using Eu.EDelivery.AS4.UnitTests.Extensions;
+using Eu.EDelivery.AS4.UnitTests.Model.PMode;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.UnitTests.Transformers
+{
+    /// <summary>
+    /// Testing <see cref="PModeToPullMessageTransformer" />
+    /// </summary>
+    public class GivenPModeToPullMessageTransformerFacts
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GivenPModeToPullMessageTransformerFacts"/> class.
+        /// </summary>
+        public GivenPModeToPullMessageTransformerFacts()
+        {
+            IdentifierFactory.Instance.SetContext(StubConfig.Instance);
+        }
+
+        [Theory]
+        [ClassData(typeof(ReceivedPullMessageSource))]
+        public async Task FailsWithNoPullConfigurationSection(ReceivedMessage receivedMessage)
+        {
+            // Arrange
+            var transformer = new PModeToPullMessageTransformer();
+
+            // Act
+            InternalMessage message = await transformer.TransformAsync(receivedMessage, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(message.Exception);
+            Assert.NotEmpty(message.Exception.Message);
+        }
+
+        [Fact]
+        public async Task SucceedsWithAValidPullConfiguration()
+        {
+            // Arrange
+            const string expectedMpc = "expected-mpc";
+            var transformer = new PModeToPullMessageTransformer();
+            var sendingPMode = new ValidStubSendingPMode("expected-id") {MessagePackaging = {Mpc = expectedMpc}};
+            var receivedMessage = new ReceivedPullMessage(new AS4Message().ToStream(), Constants.ContentTypes.Soap, sendingPMode);
+
+            // Act
+            InternalMessage message = await transformer.TransformAsync(receivedMessage, CancellationToken.None);
+
+            // Assert
+            var actualSignalMessage = message.AS4Message.PrimarySignalMessage as PullRequest;
+            Assert.Equal(expectedMpc, actualSignalMessage?.Mpc);
+        }
+
+        /// <summary>
+        /// Source of different <see cref="ReceivedPullMessage"/> instances.
+        /// </summary>
+        private class ReceivedPullMessageSource : IEnumerable<object[]>
+        {
+            /// <summary>
+            /// Returns an enumerator that iterates through a collection.
+            /// </summary>
+            /// <returns>An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.</returns>
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            /// <summary>
+            /// Returns an enumerator that iterates through the collection.
+            /// </summary>
+            /// <returns>An enumerator that can be used to iterate through the collection.</returns>
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] {new ReceivedMessage(requestStream: null)};
+                yield return new object[] {new ReceivedMessage(Stream.Null)};
+
+                var invalidSendingPMode = new ValidStubSendingPMode("my id") {PullConfiguration = new PullConfiguration()};
+                MemoryStream messageStream = new AS4Message {SendingPMode = invalidSendingPMode}.ToStream();
+
+                yield return new object[] {new ReceivedPullMessage(messageStream, Constants.ContentTypes.Soap, invalidSendingPMode) };
+            }
+        }
+    }
+}
--- source/AS4/Eu.EDelivery.AS4/Transformers/ITransformer.cs
+++ source/AS4/Eu.EDelivery.AS4/Transformers/ITransformer.cs
@@ -4,8 +4,17 @@ using Eu.EDelivery.AS4.Model.Internal;
 
 namespace Eu.EDelivery.AS4.Transformers
 {
+    /// <summary>
+    /// Contract to transform from a non-canonical format to a known canonical format inside the AS4 component.
+    /// </summary>
     public interface ITransformer
     {
+        /// <summary>
+        /// Transform a given <see cref="ReceivedMessage"/> to a Canonical <see cref="InternalMessage"/> instance.
+        /// </summary>
+        /// <param name="message">Given message to transform.</param>
+        /// <param name="cancellationToken">Cancellation which stops the transforming.</param>
+        /// <returns></returns>
         Task<InternalMessage> TransformAsync(ReceivedMessage message, CancellationToken cancellationToken);
     }
 }
