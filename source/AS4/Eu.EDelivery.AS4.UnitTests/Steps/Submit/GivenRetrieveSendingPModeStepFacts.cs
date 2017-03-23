be4aeb069e9ceea9185f4a71d67213e2f14ef6cc
Stijn Moreels <stijn.moreels@codit.eu>
2017-03-23 10:40:13 +0100

    Create 'Pull Request Transformer'

--- source/AS4/Eu.EDelivery.AS4.UnitTests/Steps/Submit/GivenRetrieveSendingPModeStepFacts.cs
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Steps/Submit/GivenRetrieveSendingPModeStepFacts.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Eu.EDelivery.AS4.Common;
 using Eu.EDelivery.AS4.Exceptions;
+using Eu.EDelivery.AS4.Model;
 using Eu.EDelivery.AS4.Model.Common;
 using Eu.EDelivery.AS4.Model.Internal;
 using Eu.EDelivery.AS4.Model.PMode;
@@ -11,8 +11,10 @@ using Eu.EDelivery.AS4.Model.Submit;
 using Eu.EDelivery.AS4.Steps;
 using Eu.EDelivery.AS4.Steps.Submit;
 using Eu.EDelivery.AS4.UnitTests.Common;
+using Eu.EDelivery.AS4.UnitTests.Model.PMode;
 using Moq;
 using Xunit;
+using CollaborationInfo = Eu.EDelivery.AS4.Model.Common.CollaborationInfo;
 
 namespace Eu.EDelivery.AS4.UnitTests.Steps.Submit
 {
@@ -27,37 +29,20 @@ namespace Eu.EDelivery.AS4.UnitTests.Steps.Submit
 
         public GivenRetrieveSendingPModeStepFacts()
         {
-            _step = new RetrieveSendingPModeStep(StubConfig.Instance);
-            _pmodeId = "01-pmode";
-            _mockedConfig = new Mock<IConfig>();
-            _mockedConfig.Setup(c => c.GetSendingPMode(It.IsAny<string>())).Returns(GetStubWrongProcessingMode());
+            this._step = new RetrieveSendingPModeStep(StubConfig.Instance);
+            this._pmodeId = "01-pmode";
+            this._mockedConfig = new Mock<IConfig>();
+            this._mockedConfig
+                .Setup(c => c.GetSendingPMode(It.IsAny<string>()))
+                .Returns(GetStubWrongProcessingMode());
         }
 
         private SendingProcessingMode GetStubWrongProcessingMode()
         {
             Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-            return new SendingProcessingMode {Id = _pmodeId};
-        }
-
-        private SendingProcessingMode GetStubRightProcessingMode()
-        {
-            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
             return new SendingProcessingMode
             {
-                Id = _pmodeId,
-                PushConfiguration = new PushConfiguration {Protocol = new Protocol {Url = "http://127.0.0.1/msh"}},
-                Security =
-                    new AS4.Model.PMode.Security
-                    {
-                        Signing =
-                            new Signing
-                            {
-                                PrivateKeyFindValue = "My",
-                                PrivateKeyFindType = X509FindType.FindBySubjectName,
-                                Algorithm = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
-                                HashFunction = "http://www.w3.org/2001/04/xmlenc#sha256"
-                            }
-                    }
+                Id = this._pmodeId
             };
         }
 
@@ -65,7 +50,13 @@ namespace Eu.EDelivery.AS4.UnitTests.Steps.Submit
         {
             return new SubmitMessage
             {
-                Collaboration = new CollaborationInfo {AgreementRef = new Agreement {PModeId = _pmodeId}}
+                Collaboration = new CollaborationInfo
+                {
+                    AgreementRef = new Agreement
+                    {
+                        PModeId = this._pmodeId
+                    }
+                }
             };
         }
 
@@ -79,29 +70,30 @@ namespace Eu.EDelivery.AS4.UnitTests.Steps.Submit
             public async Task ThenExecuteMethodRetrievesPModeFailsWithInvalidPModeAsync()
             {
                 // Arrange
-                var internalMessage = new InternalMessage(GetStubSubmitMessage());
-                _step = new RetrieveSendingPModeStep(_mockedConfig.Object);
-
+                var internalMessage = new InternalMessage(base.GetStubSubmitMessage());
+                base._step = new RetrieveSendingPModeStep(base._mockedConfig.Object);
                 // Act / Assert
-                await Assert.ThrowsAsync<AS4Exception>(
-                    () => _step.ExecuteAsync(internalMessage, CancellationToken.None));
+                await
+                    Assert.ThrowsAsync<AS4Exception>(
+                        () => base._step.ExecuteAsync(internalMessage, CancellationToken.None));
             }
 
             [Fact]
             public async Task ThenExecuteRetrievesPModeSucceedsWithTwoStepsAsync()
             {
                 // Arrange
-                var internalMessage = new InternalMessage(GetStubSubmitMessage());
-                _mockedConfig = new Mock<IConfig>();
-                _mockedConfig.Setup(c => c.GetSendingPMode(It.IsAny<string>())).Returns(GetStubRightProcessingMode());
-
+                var internalMessage = new InternalMessage(base.GetStubSubmitMessage());
+                base._mockedConfig = new Mock<IConfig>();
+                base._mockedConfig
+                    .Setup(c => c.GetSendingPMode(It.IsAny<string>()))
+                    .Returns(new ValidStubSendingPMode(_pmodeId));
                 // Act
-                var step1 = new RetrieveSendingPModeStep(_mockedConfig.Object);
-                var step2 = new RetrieveSendingPModeStep(_mockedConfig.Object);
+                var step1 = new RetrieveSendingPModeStep(base._mockedConfig.Object);
+                var step2 = new RetrieveSendingPModeStep(base._mockedConfig.Object);
 
                 StepResult result1 = null, result2 = null;
                 await Task.Run(async () => result1 = await step1.ExecuteAsync(internalMessage, CancellationToken.None));
-                await Task.Run(async () => result2 = await step2.ExecuteAsync(internalMessage, CancellationToken.None));
+                await Task.Run(async() => result2 = await step2.ExecuteAsync(internalMessage, CancellationToken.None));
 
                 // Assert
                 Assert.NotNull(result1);
