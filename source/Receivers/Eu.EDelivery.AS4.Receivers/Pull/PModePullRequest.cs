b4d6382bc060661c89c467b256d288a3ca64b4ef
Stijn Moreels <stijn.moreels@codit.eu>
2017-03-24 13:45:32 +0100

    Update with remarks + add extension methods for 'XmlAttribute' instances

---
+++ source/Receivers/Eu.EDelivery.AS4.Receivers/Pull/PModePullRequest.cs
@@ -0,0 +1,26 @@
+using System;
+using Eu.EDelivery.AS4.Model.Core;
+using Eu.EDelivery.AS4.Model.PMode;
+
+namespace Eu.EDelivery.AS4.Receivers.Pull
+{
+    /// <summary>
+    /// <see cref="IntervalRequest"/> implementation to request a <see cref="PullRequest"/> for a given <see cref="SendingProcessingMode"/>.
+    /// </summary>
+    public class PModePullRequest : IntervalRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PModePullRequest"/> class.
+        /// </summary>
+        /// <param name="pmode">The pmode.</param>
+        /// <param name="minInterval">The min Interval.</param>
+        /// <param name="maxInterval">The max Interval.</param>
+        public PModePullRequest(SendingProcessingMode pmode, TimeSpan minInterval, TimeSpan maxInterval)
+            : base(minInterval, maxInterval)
+        {
+            PMode = pmode;
+        }
+
+        public SendingProcessingMode PMode { get; }
+    }
+}
--- source/Receivers/Eu.EDelivery.AS4.Receivers/PullRequestReceiver.cs
+++ source/Receivers/Eu.EDelivery.AS4.Receivers/PullRequestReceiver.cs
@@ -1,32 +1,24 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Timers;
 using Eu.EDelivery.AS4.Common;
+using Eu.EDelivery.AS4.Extensions;
 using Eu.EDelivery.AS4.Model.Internal;
 using Eu.EDelivery.AS4.Model.PMode;
 using Eu.EDelivery.AS4.Receivers.Pull;
 using Eu.EDelivery.AS4.Serialization;
-using Timer = System.Timers.Timer;
 
 namespace Eu.EDelivery.AS4.Receivers
 {
     /// <summary>
     /// <see cref="IReceiver"/> implementation to pull exponentially for Pull Requests.
     /// </summary>
-    public class PullRequestReceiver : IReceiver
+    public class PullRequestReceiver : ExponentialIntervalReceiver<PModePullRequest>
     {
         private readonly IConfig _configuration;
-        private readonly Timer _timer;
-        private readonly IDictionary<DateTime, List<PModeRequest>> _runSchedulePModes;
-        private readonly ICollection<PModeRequest> _pmodeRequests;
 
-        private Func<PModeRequest, Task<InternalMessage>> _messageCallback;
-        private bool _running;
+        private Func<PModePullRequest, Task<InternalMessage>> _messageCallback;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PullRequestReceiver"/> class.
@@ -40,74 +32,22 @@ namespace Eu.EDelivery.AS4.Receivers
         public PullRequestReceiver(IConfig configuration)
         {
             _configuration = configuration;
-            _timer = new Timer();
-            _runSchedulePModes = new ConcurrentDictionary<DateTime, List<PModeRequest>>();
-            _pmodeRequests = new List<PModeRequest>();
         }
 
         /// <summary>
         /// Configure the receiver with a given settings dictionary.
         /// </summary>
         /// <param name="settings"></param>
-        public void Configure(IEnumerable<Setting> settings)
+        public override void Configure(IEnumerable<Setting> settings)
         {
             foreach (Setting setting in settings)
             {
+                if (!_configuration.ContainsSendingPMode(setting.Key)) continue;
+
                 SendingProcessingMode pmode = _configuration.GetSendingPMode(setting.Key);
-                _pmodeRequests.Add(new PModeRequest(pmode, setting["tmin"], setting["tmax"]));
-            }
+                var pullRequest = new PModePullRequest(pmode, setting["tmin"].AsTimeSpan(), setting["tmax"].AsTimeSpan());
 
-            _timer.Elapsed += TimerEnlapsed;
-        }
-
-        private void TimerEnlapsed(object sender, ElapsedEventArgs eventArgs)
-        {
-            _timer.Stop();
-
-            if (!_running)
-            {
-                return;
-            }
-
-            List<PModeRequest> pmodeRequests = SelectAllPModeRequestsForThisEvent(eventArgs);
-            RemoveAllSelectedPModeRequestsForThisEvent(eventArgs);
-
-            WaitForAllRequests(pmodeRequests);
-            DetermineNextRuns(pmodeRequests);
-        }
-
-        private List<PModeRequest> SelectAllPModeRequestsForThisEvent(ElapsedEventArgs eventArgs)
-        {
-            return _runSchedulePModes.Where(s => s.Key <= eventArgs.SignalTime).SelectMany(p => p.Value).ToList();
-        }
-
-        private void RemoveAllSelectedPModeRequestsForThisEvent(ElapsedEventArgs eventArgs)
-        {
-            List<DateTime> keys = _runSchedulePModes.Keys.Where(k => k <= eventArgs.SignalTime).ToList();
-            keys.ForEach(k => _runSchedulePModes.Remove(k));
-        }
-
-        private void WaitForAllRequests(IEnumerable<PModeRequest> pmodeRequests)
-        {
-            var tasks = new List<Task>();
-
-            foreach (PModeRequest pmodeRequest in pmodeRequests)
-            {
-                pmodeRequest.CalculateNewInterval();
-
-                tasks.Add(Task.Run(() => OnPModeReceived(pmodeRequest)));
-            }
-
-            Task.WaitAll(tasks.ToArray());
-        }
-
-        private async Task OnPModeReceived(PModeRequest pmodeRequest)
-        {
-            InternalMessage resultedMessage = await _messageCallback(pmodeRequest);
-
-            if (resultedMessage.AS4Message.IsUserMessage)
-            {
-                pmodeRequest.ResetInterval();
+                base.AddIntervalRequest(pullRequest);
             }
         }
 
@@ -117,7 +57,7 @@ namespace Eu.EDelivery.AS4.Receivers
         /// </summary>
         /// <param name="messageCallback"></param>
         /// <param name="cancellationToken"></param>
-        public void StartReceiving(
+        public override void StartReceiving(
             Func<ReceivedMessage, CancellationToken, Task<InternalMessage>> messageCallback,
             CancellationToken cancellationToken)
         {
@@ -127,59 +67,19 @@ namespace Eu.EDelivery.AS4.Receivers
                 return messageCallback(receivedMessage, cancellationToken);
             };
 
-            _running = true;
-
-            DetermineNextRuns(_pmodeRequests);
-        }
-
-        private void DetermineNextRuns(IEnumerable<PModeRequest> pmodeRequests)
-        {
-            DateTime currentTime = DateTime.Now;
-
-            foreach (PModeRequest pmodeRequest in pmodeRequests)
-            {
-                AddPModeRequest(currentTime + pmodeRequest.CurrentInterval, pmodeRequest);
-            }
-
-            if (!_runSchedulePModes.Any()) return;
-
-            _timer.Interval = CalculateNextTriggerInterval(currentTime);
-            _timer.Start();
-        }
-
-        private void AddPModeRequest(DateTime dateTime, PModeRequest pmodeRequest)
-        {
-            var timeTrimmedOnSeconds = new DateTime(
-                dateTime.Year,
-                dateTime.Month,
-                dateTime.Day,
-                dateTime.Hour,
-                dateTime.Minute,
-                dateTime.Second);
-
-            if (_runSchedulePModes.ContainsKey(timeTrimmedOnSeconds) == false)
-            {
-                _runSchedulePModes.Add(timeTrimmedOnSeconds, new List<PModeRequest>());
-            }
-
-            _runSchedulePModes[timeTrimmedOnSeconds].Add(pmodeRequest);
-        }
-
-        private double CalculateNextTriggerInterval(DateTime now)
-        {
-            DateTime firstRunDate = _runSchedulePModes.Min(t => t.Key);
-            TimeSpan triggerTime = firstRunDate - now;
-
-            return triggerTime.TotalMilliseconds <= 0 ? 1 : triggerTime.TotalMilliseconds;
+            base.StartInterval();
         }
 
         /// <summary>
-        /// Stop the given receiver from pulling Pull Requests.
+        /// <paramref name="intervalPullRequest"/> is received.
         /// </summary>
-        public void StopReceiving()
+        /// <param name="intervalPullRequest"></param>
+        /// <returns></returns>
+        protected override async Task<bool> OnRequestReceived(PModePullRequest intervalPullRequest)
         {
-            _running = false;
-            _timer.Dispose();
+            InternalMessage resultedMessage = await _messageCallback(intervalPullRequest);
+
+            return resultedMessage.AS4Message.IsUserMessage;
         }
     }
 }
