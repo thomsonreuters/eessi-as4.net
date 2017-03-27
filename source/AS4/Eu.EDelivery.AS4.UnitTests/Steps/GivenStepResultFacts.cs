77d51bd965e8f023db9abaa41b9021052bc00113
Stijn Moreels <stijn.moreels@codit.eu>
2017-03-27 14:40:05 +0200

    Test if the PullRequest and Error response cause the Step execution to stop

--- source/AS4/Eu.EDelivery.AS4.UnitTests/Steps/GivenStepResultFacts.cs
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Steps/GivenStepResultFacts.cs
@@ -1,5 +1,7 @@
 ﻿using Eu.EDelivery.AS4.Builders.Core;
 using Eu.EDelivery.AS4.Exceptions;
+using Eu.EDelivery.AS4.Model.Core;
+using Eu.EDelivery.AS4.Model.Internal;
 using Eu.EDelivery.AS4.Steps;
 using Xunit;
 
@@ -23,6 +25,19 @@ namespace Eu.EDelivery.AS4.UnitTests.Steps
 
                 // Assert
                 Assert.False(actualStepResult.CanExecute);
+            }
+
+            [Fact]
+            public void IsTrueIfStopExecutiongIsntCalled()
+            {
+                // Arrange
+                AS4Message as4Message = new AS4MessageBuilder().Build();
+
+                // Act
+                StepResult actualStepResult = StepResult.Success(new InternalMessage(as4Message));
+
+                // Assert
+                Assert.True(actualStepResult.CanExecute);
             }
         }
     }
--- source/AS4/Eu.EDelivery.AS4/Model/Core/Error.cs
+++ source/AS4/Eu.EDelivery.AS4/Model/Core/Error.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Serialization;
 using Eu.EDelivery.AS4.Exceptions;
 
@@ -6,9 +7,11 @@ namespace Eu.EDelivery.AS4.Model.Core
 {
     public class Error : SignalMessage
     {
-        [XmlIgnore] public AS4Exception Exception { get; set; }
-        [XmlIgnore] public bool IsFormedByException => this.Exception != null;
-        
+        [XmlIgnore]
+        public AS4Exception Exception { get; set; }
+
+        [XmlIgnore]
+        public bool IsFormedByException => this.Exception != null;
 
         public IList<ErrorDetail> Errors { get; set; }
 
@@ -24,6 +27,24 @@ namespace Eu.EDelivery.AS4.Model.Core
         /// <param name="messageId"></param>
         public Error(string messageId) : base(messageId) {}
 
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="Error"/> is originated from a Pull Request.
+        /// </summary>
+        [XmlIgnore]
+        public bool FromPullRequest
+        {
+            get
+            {
+                ErrorDetail firstPullRequestError =
+                    Errors.FirstOrDefault(
+                        detail =>
+                            detail.Severity == Severity.WARNING
+                            && detail.ShortDescription.Equals("EmptyMessagePartitionChannel"));
+
+                return firstPullRequestError != null;
+            }
+        }
+
         public override string GetActionValue()
         {
             return "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/oneWay.error";
@@ -33,18 +54,26 @@ namespace Eu.EDelivery.AS4.Model.Core
     public class ErrorDescription
     {
         public string Language { get; set; }
+
         public string Value { get; set; }
     }
 
     public class ErrorDetail
     {
         public string ErrorCode { get; set; }
+
         public Severity Severity { get; set; }
+
         public string Origin { get; set; }
+
         public string Category { get; set; }
+
         public string RefToMessageInError { get; set; }
+
         public string ShortDescription { get; set; }
+
         public ErrorDescription Description { get; set; }
+
         public string Detail { get; set; }
     }
 
