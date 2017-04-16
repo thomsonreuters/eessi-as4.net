277c03cf72b2f566a77460db66ca7074d14ce899
Stijn Moreels <stijn.moreels@codit.eu>
2017-04-16 10:31:04 +0200

    Update build

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-tools\FAKE\FAKE.exe build.fsx
+@echo off
+tools\FAKE\Fake.exe build.fsx "%1"
+exit /b %errorlevel%
--- build.fsx
+++ build.fsx
@@ -2,46 +2,49 @@
 
 open Fake
 open Fake.Testing
-open Fake.OpenCoverHelper
+open Fake.DotCover
 
+/// <summary>
+/// Compile the Solution with a 'Release' configuraiton.
+/// </summary>
 Target "Compile" (fun _ -> 
     let buildMode = getBuildParamOrDefault "buildMode" "Release"
     let setParams defaults =
         { defaults with
             Verbosity = Some(Quiet)
             Targets = ["Build"]
-            Properties =
-                [
-                    "Optimize", "True"
-                    "DebugSymbols", "True"
-                    "Configuration", buildMode
-                ]
+            Properties = [ "Optimize", "True"; "DebugSymbols", "True"; "Configuration", buildMode]
          }
-    build setParams "./source/AS4.sln"
-          |> DoNothing
+    build setParams "./source/AS4.sln" |> DoNothing
 )
 
-Target "Unit Tests" (fun _ ->
-    ["./output/Eu.EDelivery.AS4.UnitTests.dll"; "./output/Eu.EDelivery.AS4.Fe.UnitTests.dll"]
-        |> xUnit2 (fun p ->
-            {p with
-                ShadowCopy = false;
-                Parallel = ParallelMode.NoParallelization;
-                HtmlOutputPath = Some("./tests.html");
-                XmlOutputPath = Some("./tests.xml")})
-)
+let testAssemblies = ["./output/Eu.EDelivery.AS4.UnitTests.dll"; "./output/Eu.EDelivery.AS4.Fe.UnitTests.dll"]
+let xUnitParams p = { p with ShadowCopy = false; Parallel = ParallelMode.All;}
 
+/// <summary>
+/// Test the 'Unit Test' assemblies.
+/// </summary>
+Target "UnitTests" (fun _ -> testAssemblies |> xUnit2 xUnitParams)
+
+/// <summary>
+/// Coverage the Solution with the Unit Tests.
+/// </summary>
 Target "Coverage" (fun _ ->
-    OpenCover (fun p -> 
-        {p with     
-            TestRunnerExePath = "./source/packages/xunit.runner.console.2.2.0/tools/xunit-console.exe";
-            ExePath = "./tools/OpenCover/tools/OpenCover.Console.exe";
-            Register = RegisterUser;
-            Output = "coverage.xml";
-            Filter = "+[*]* -[Test*]*"}) "./output/Eu.EDelivery.AS4.UnitTests.dll"
-)
+    let dotCoverSnapShot = "./output/dotCoverSnapshot.dcvr"
 
-"Compile" 
-    ==> "Unit Tests"
+    let dotCoverParams defaults = {defaults with TargetExecutable = "./source/packages/xunit.runner.console.2.2.0/tools/xunit.console.exe"; Output = dotCoverSnapShot; Filters = "-:FluentValidation"}
+    testAssemblies |> DotCoverXUnit2 dotCoverParams xUnitParams
 
-RunTargetOrDefault "Unit Tests"
+    let dotReportParams defaults = {defaults with Source = dotCoverSnapShot; Output = "./output/dotCoverReport.html"; ReportType = DotCoverReportType.Html;}
+    DotCoverReport dotReportParams true)
+
+/// <summary>
+/// Inspect the Solution with FxCop.
+/// </summary>
+Target "Inspect" (fun _ -> !! ("./output/Eu.EDelivery.AS4**.dll") |> FxCop (fun p -> {p with ReportFileName = "./output/FxCopResult.xml"; ToolPath = "./tools/FxCop/FxCopCmd.exe";}))
+
+"Compile" ==> "Coverage"
+"Compile" ==> "UnitTests"
+"Compile" ==> "Inspect"
+
+RunTargetOrDefault "Coverage"
