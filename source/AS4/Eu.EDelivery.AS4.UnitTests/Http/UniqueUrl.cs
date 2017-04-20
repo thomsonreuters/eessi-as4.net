127e68b919a8beda6343e911cce99590e37ca24f
Stijn Moreels <stijn.moreels@codit.eu>
2017-04-20 08:44:14 +0200

    Add 'IDisposable' to 'HttpReceiver' + Try different approach for open TCP ports in Tests

--- source/AS4/Eu.EDelivery.AS4.UnitTests/Http/UniqueUrl.cs
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Http/UniqueUrl.cs
@@ -1,23 +1,25 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace Eu.EDelivery.AS4.UnitTests.Http
 {
     public static class UniqueHost
     {
-        private static readonly Random Random;
-
-        static UniqueHost()
-        {
-            Random = new Random();
-        }
-
         /// <summary>
         /// Create a new instance of the <see cref="UniqueHost" /> class.
         /// </summary>
         /// <returns></returns>
         public static string Create()
         {
-            return $"http://localhost:{Random.Next(4700, 4800)}";
+            return $"http://localhost:{GetOpenPort()}";
+        }
+
+        public static int GetOpenPort(int startPort = 2555)
+        {
+            List<int> usedPorts = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners().Select(p => p.Port).ToList();
+
+            return Enumerable.Range(startPort, 99).FirstOrDefault(port => !usedPorts.Contains(port));
         }
     }
 }
