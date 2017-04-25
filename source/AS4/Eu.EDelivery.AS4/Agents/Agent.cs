363bb9983ef14d1ff56f8d7369d94713693ab833
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-04-25 16:45:01 +0200

    Naming: async methods suffixed with Async

--- source/AS4/Eu.EDelivery.AS4/Agents/Agent.cs
+++ source/AS4/Eu.EDelivery.AS4/Agents/Agent.cs
@@ -175,7 +175,7 @@ namespace Eu.EDelivery.AS4.Agents
             sw.Start();
 #endif
 
-            InternalMessage internalMessage = await TryTransform(message, cancellationToken);
+            InternalMessage internalMessage = await TryTransformAsync(message, cancellationToken);
 
             if (internalMessage.Exception != null)
             {
@@ -195,7 +195,7 @@ namespace Eu.EDelivery.AS4.Agents
             return result.InternalMessage;
         }
 
-        private async Task<InternalMessage> TryTransform(ReceivedMessage message, CancellationToken cancellationToken)
+        private async Task<InternalMessage> TryTransformAsync(ReceivedMessage message, CancellationToken cancellationToken)
         {
             try
             {
