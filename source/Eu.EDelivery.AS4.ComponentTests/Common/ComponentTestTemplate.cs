1d149ff71345071720844a91b1f2171e90269576
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-04-28 13:37:48 +0200

    Moved the component-tests to a seperate project.

---
+++ source/Eu.EDelivery.AS4.ComponentTests/Common/ComponentTestTemplate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.ComponentTests.Common
+{
+    [Collection("ComponentTest")]
+    public class ComponentTestTemplate : IDisposable
+    {
+        private bool _restoreSettings = false;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComponentTestTemplate"/> class.
+        /// </summary>
+        public ComponentTestTemplate()
+        {
+            CopyDirectory(@".\config\componenttest-pmodes\send-pmodes", @".\config\send-pmodes");
+        }
+
+        protected void OverrideSettings(string settingsFile)
+        {
+            File.Copy(@".\config\settings.xml", @".\config\settings_original.xml", true);
+            File.Copy(settingsFile, @".\config\settings.xml", true);
+            _restoreSettings = true;
+        }
+
+        public void Dispose()
+        {
+            Disposing(true);
+            if (_restoreSettings && File.Exists(@".\config\settings_original.xml"))
+            {
+                File.Copy(@".\config\settings_original.xml", @".\config\settings.xml", true);
+            }
+        }
+
+        protected virtual void Disposing(bool isDisposing) { }
+
+        #region Infrastructure methods
+
+        private static void CopyDirectory(string sourceDirName, string destDirName)
+        {
+            if (Directory.Exists(sourceDirName) == false)
+            {
+                throw new DirectoryNotFoundException($"The {sourceDirName} directory can not be found.");
+            }
+
+            if (Directory.Exists(destDirName) == false)
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            var files = Directory.GetFiles(sourceDirName);
+
+            foreach (string fileName in files)
+            {
+                File.Copy(fileName, Path.Combine(destDirName, Path.GetFileName(fileName)), true);
+            }
+        }
+
+        #endregion
+    }
+}
