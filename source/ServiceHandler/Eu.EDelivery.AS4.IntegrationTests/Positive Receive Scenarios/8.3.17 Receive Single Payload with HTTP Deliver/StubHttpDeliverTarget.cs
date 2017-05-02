3d2011d02d74f97d572ef96b242c53b09b5df94f
Stijn Moreels <stijn.moreels@codit.eu>
2017-05-02 15:27:37 +0200

    Start new Integration Test

---
+++ source/ServiceHandler/Eu.EDelivery.AS4.IntegrationTests/Positive Receive Scenarios/8.3.17 Receive Single Payload with HTTP Deliver/StubHttpDeliverTarget.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using SimpleHttpMock;
+
+namespace Eu.EDelivery.AS4.IntegrationTests.Positive_Receive_Scenarios._8._3._17_Receive_Single_Payload_with_HTTP_Deliver
+{
+    public class StubHttpDeliverTarget : IDisposable
+    {
+        private MockedHttpServer _httpServer;
+
+        /// <summary>
+        /// Gets the Deliver Message Content
+        /// </summary>
+        public string DeliverMessageContent { get; private set; }
+
+        /// <summary>
+        /// Create Stub target at a given <paramref name="location"/>.
+        /// </summary>
+        /// <param name="location">The HTTP Location.</param>
+        /// <returns></returns>
+        public static StubHttpDeliverTarget AtLocation(string location)
+        {
+            var builder = new MockedHttpServerBuilder();
+            var target = new StubHttpDeliverTarget();
+
+            builder.WhenPost(location).RespondContent(
+                httpStatusCode: HttpStatusCode.OK,
+                contentFn: request =>
+                {
+                    Task<string> task = request.Content.ReadAsStringAsync();
+                    Task.WhenAll(task).ContinueWith(t => target.DeliverMessageContent = task.Result).Wait();
+
+                    return null;
+                });
+
+            target._httpServer = builder.Build(location);
+            return target;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            _httpServer.Dispose();
+        }
+    }
+}
---
+++ source/ServiceHandler/Eu.EDelivery.AS4.IntegrationTests/Positive Receive Scenarios/8.3.17 Receive Single Payload with HTTP Deliver/TestSubmitDeliverSinglePayloadViaHttp.cs
@@ -0,0 +1,19 @@
+﻿using Eu.EDelivery.AS4.IntegrationTests.Common;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.IntegrationTests.Positive_Receive_Scenarios._8._3._17_Receive_Single_Payload_with_HTTP_Deliver
+{
+    public class TestSubmitDeliverSinglePayloadViaHttp : IntegrationTestTemplate
+    {
+        [Fact]
+        public void RunIntegrationTest()
+        {
+            // Before
+            const string location = "http://location:5000";
+            using (StubHttpDeliverTarget deliverTarget = StubHttpDeliverTarget.AtLocation(location))
+            {
+                
+            }
+        }
+    }
+}
