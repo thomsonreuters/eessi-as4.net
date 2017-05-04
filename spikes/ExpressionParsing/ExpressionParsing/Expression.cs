db8d608c8f4179214b812323d65a40f4acb953d6
Stijn Moreels <stijn.moreels@codit.eu>
2017-05-04 10:34:15 +0200

    Start new project

---
+++ spikes/ExpressionParsing/ExpressionParsing/Expression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace ExpressionParsing
+{
+    public class Expression
+    {
+        [Theory]
+        [InlineData("Operation = ToBeSent", new[] { "Operation", "=", "ToBeSent"})]
+        public void TestTokenize(string filter, string[] expectedTokens)
+        {
+            string[] actualTokens = Tokenize(filter);
+            Assert.Equal(expectedTokens, actualTokens);
+        }
+
+        public string[] Tokenize(string filter)
+        {
+            string[] result = filter.Split('=');
+            for (var i = 0; i < result.Length - 1; i++)
+            {
+                result[i] += '=';
+            }
+
+            return result;
+        }
+    }
+}
