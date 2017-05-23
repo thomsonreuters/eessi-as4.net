d9f7a0329d6f912d099cbf7f20491969975581fe
Stijn Moreels <stijn.moreels@codit.eu>
2017-05-23 11:11:12 +0200

    Update with default store location in setting files

---
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Common/GivenConfigFacts.cs
@@ -0,0 +1,17 @@
+﻿using Eu.EDelivery.AS4.Common;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.UnitTests.Common
+{
+    public class GivenConfigFacts
+    {
+        [Fact]
+        public void DefaultInOutStore()
+        {
+            // Assert
+            Assert.False(Config.Instance.IsInitialized);
+            Assert.NotEmpty(Config.Instance.InStore);
+            Assert.NotEmpty(Config.Instance.OutStore);
+        }
+    }
+}
