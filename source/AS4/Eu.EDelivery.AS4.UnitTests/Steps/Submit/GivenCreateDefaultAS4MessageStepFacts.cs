7aea32ab3328ba6622aa3d3129348db237094153
Stijn Moreels <stijn.moreels@codit.eu>
2017-05-30 08:51:45 +0200

    Add test for Default AS4 message creation

---
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Steps/Submit/GivenCreateDefaultAS4MessageStepFacts.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Eu.EDelivery.AS4.Builders.Core;
+using Eu.EDelivery.AS4.Model.Core;
+using Eu.EDelivery.AS4.Model.Internal;
+using Eu.EDelivery.AS4.Model.PMode;
+using Eu.EDelivery.AS4.Steps;
+using Eu.EDelivery.AS4.Steps.Submit;
+using Eu.EDelivery.AS4.UnitTests.Common;
+using Eu.EDelivery.AS4.UnitTests.Model.PMode;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.UnitTests.Steps.Submit
+{
+    /// <summary>
+    /// Testing <see cref="CreateDefaultAS4MessageStep"/>
+    /// </summary>
+    public class GivenCreateDefaultAS4MessageStepFacts : PseudoConfig
+    {
+        [Fact]
+        public async Task CreatesExpectedMessageFromPMode()
+        {
+            // Arrange
+            var sut = new CreateDefaultAS4MessageStep(this);
+            sut.Configure(properties: null);
+            const string attachmentId = "attachment id";
+
+            // Act
+            StepResult result = await sut.ExecuteAsync(AttachmentWithoutUserMessage(attachmentId), CancellationToken.None);
+
+            // Assert
+            AS4Message as4Message = result.InternalMessage.AS4Message;
+            Assert.Equal(1, as4Message.UserMessages.Count);
+            Assert.Contains(attachmentId, as4Message.PrimaryUserMessage.PayloadInfo.First().Href);
+        }
+
+        /// <summary>
+        /// Retrieve the PMode from the Global Settings
+        /// </summary>
+        /// <param name="id"></param>
+        /// <exception cref="Exception"></exception>
+        /// <returns></returns>
+        public override SendingProcessingMode GetSendingPMode(string id)
+        {
+            SendingProcessingMode pmode = new ValidStubSendingPModeFactory().Create();
+            pmode.MessagePackaging.MessageProperties = new List<MessageProperty>();
+
+            return pmode;
+        }
+
+        private static InternalMessage AttachmentWithoutUserMessage(string atttachmentId)
+        {
+            AS4Message as4Message = new AS4MessageBuilder().WithAttachment(new Attachment(atttachmentId)).Build();
+            return new InternalMessage(as4Message);
+        }
+    }
+}
