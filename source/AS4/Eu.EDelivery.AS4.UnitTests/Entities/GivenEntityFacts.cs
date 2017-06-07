aefef69328379e7b4084665f1cd4e2996b557004
Stijn Moreels <stijn.moreels@codit.eu>
2017-06-07 07:21:19 +0200

    Add test for entity

---
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Entities/GivenEntityFacts.cs
@@ -0,0 +1,25 @@
+﻿using Eu.EDelivery.AS4.Entities;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.UnitTests.Entities
+{
+    /// <summary>
+    /// Testing <see cref="Entity"/>
+    /// </summary>
+    public class GivenEntityFacts
+    {
+        [Fact]
+        public void IsTransient()
+        {
+            // Arrange
+            var entity = new Entity();
+            
+            // Act
+            entity.InitializeIdFromDatabase(1);
+
+            // Assert
+            Assert.False(entity.IsTransient);
+            Assert.True(new Entity().IsTransient);
+        }
+    }
+}
