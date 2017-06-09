f54ab74d80314f7ea569df80648faab282b79faa
Stijn Moreels <stijn.moreels@codit.eu>
2017-06-09 11:06:03 +0200

    Add parser

---
+++ source/Receivers/Eu.EDelivery.AS4.Receivers/Specifications/Expressions/DatastoreExpressionParser.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace Eu.EDelivery.AS4.Receivers.Specifications.Expressions
+{
+    internal sealed class DatastoreExpressionParser
+    {
+        /// <summary>
+        /// Evaluates the specified database set.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="databaseSet">The database set.</param>
+        /// <param name="tokenExpression">The token expression.</param>
+        /// <returns></returns>
+        public static bool Evaluate<T>(T databaseSet, IReadOnlyCollection<Token> tokenExpression)
+        {
+            var tokenResult = new Stack<Token>();
+
+            foreach (Token token in tokenResult)
+            {
+                if (token.IsClosedParenthesis)
+                {
+                    tokenResult.Push(EvaluateEverythingInsideParenthesis(tokenResult));
+                }
+                else
+                {
+                    tokenResult.Push(
+                        token.IsEqualExpression
+                            ? Token.CreateToken(
+                                EqualExpression<T>.For(token.Expression, databaseSet).Evaluate().ToString())
+                            : token);
+                }
+            }
+
+            return LogicalEval(tokenResult.ToArray()).Evaluate;
+        }
+
+        private static Token EvaluateEverythingInsideParenthesis(Stack<Token> stack)
+        {
+            var everythingInParenthesis = new List<Token>();
+            Token nextToken = stack.Pop();
+
+            while (!nextToken.IsOpenParenthesis)
+            {
+                everythingInParenthesis.Add(nextToken);
+                nextToken = stack.Pop();
+            }
+
+            return LogicalEval(everythingInParenthesis.ToArray());
+        }
+
+        private static Token LogicalEval(IEnumerable<Token> expression)
+        {
+            var stack = new Stack<Token>();
+
+            foreach (Token token in expression)
+            {
+                stack.Push(token);
+
+                if (stack.Count == 3)
+                {
+                    stack.Push(EvaluateBooleanExpression(stack));
+                }
+            }
+
+            return stack.Pop();
+        }
+
+        private static Token EvaluateBooleanExpression(Stack<Token> stack)
+        {
+            string leftValue = stack.Pop().Expression;
+            string operatorValue = stack.Pop().Expression;
+            string rightValue = stack.Pop().Expression;
+
+            string expression = BooleanExpression
+                .For(operatorValue, leftValue, rightValue)
+                .Evaluate()
+                .ToString();
+
+            return Token.CreateToken(expression);
+        }
+    }
+}
