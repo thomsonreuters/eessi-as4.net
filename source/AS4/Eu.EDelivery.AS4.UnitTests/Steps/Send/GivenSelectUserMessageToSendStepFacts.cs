d0246d4be38f2ae527a09bfef41ee2eed677c0a7
Stijn Moreels <stijn.moreels@codit.eu>
2017-06-12 14:43:05 +0200

    Added step

---
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Steps/Send/GivenSelectUserMessageToSendStepFacts.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Eu.EDelivery.AS4.Builders.Core;
+using Eu.EDelivery.AS4.Common;
+using Eu.EDelivery.AS4.Entities;
+using Eu.EDelivery.AS4.Model.Core;
+using Eu.EDelivery.AS4.Model.Internal;
+using Eu.EDelivery.AS4.Steps;
+using Eu.EDelivery.AS4.Steps.Send;
+using Eu.EDelivery.AS4.UnitTests.Common;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.UnitTests.Steps.Send
+{
+    /// <summary>
+    /// Testing <see cref="SelectUserMessageToSendStep"/>
+    /// </summary>
+    public class GivenSelectUserMessageToSendStepFacts : GivenDatastoreFacts
+    {
+        [Fact]
+        public async Task SelectsUserMessage_IfUserMessageMatchesCriteria()
+        {
+            // Arrange
+            const string expectedId = "message-id", expectedMpc = "message-mpc";
+            InsertUserMessageForPullRequest(expectedId, expectedMpc);
+
+            var sut = new SelectUserMessageToSendStep(GetDataStoreContext);
+            MessagingContext context = ContextWithPullRequest(expectedId, expectedMpc);
+
+            // Act
+            StepResult result = await sut.ExecuteAsync(context, CancellationToken.None);
+
+            // Assert
+            UserMessage userMessage = result.MessagingContext.AS4Message.PrimaryUserMessage;
+            Assert.Equal(expectedId, userMessage.MessageId);
+            Assert.Equal(expectedMpc, userMessage.Mpc);
+        }
+
+        private void InsertUserMessageForPullRequest(string expectedId, string expectedMpc)
+        {
+            InsertOutMessage(
+                m =>
+                {
+                    m.EbmsMessageId = expectedId;
+                    m.Operation = Operation.ToBeSent;
+                    m.Mpc = expectedMpc;
+                    m.MEP = MessageExchangePattern.Pull;
+                });
+        }
+
+        private void InsertOutMessage(Action<OutMessage> arrangeMessage)
+        {
+            using (DatastoreContext context = GetDataStoreContext())
+            {
+                var message = new OutMessage();
+                arrangeMessage(message);
+
+                context.OutMessages.Add(message);
+                context.SaveChanges();
+            }
+        }
+
+        private static MessagingContext ContextWithPullRequest(string id, string mpc)
+        {
+            var pullRequest = new PullRequest(mpc, id);
+            AS4Message as4Message = new AS4MessageBuilder().WithSignalMessage(pullRequest).Build();
+
+            return new MessagingContext(as4Message);
+        }
+    }
+}
---
+++ source/Steps/Eu.EDelivery.AS4.Steps/Send/SelectUserMessageToSendStep.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Eu.EDelivery.AS4.Common;
+using Eu.EDelivery.AS4.Model.Internal;
+
+namespace Eu.EDelivery.AS4.Steps.Send
+{
+    /// <summary>
+    /// Describes how a MessageUnit should be selected to be sent via Pulling.
+    /// </summary>
+    /// <seealso cref="IStep" />
+    public class SelectUserMessageToSendStep : IStep
+    {
+        private readonly Func<DatastoreContext> _createContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SelectUserMessageToSendStep" /> class.
+        /// </summary>
+        /// <param name="createContext">The create context.</param>
+        public SelectUserMessageToSendStep(Func<DatastoreContext> createContext)
+        {
+            _createContext = createContext;
+        }
+
+        /// <summary>
+        /// Execute the step for a given <paramref name="messagingContext"/>.
+        /// </summary>
+        /// <param name="messagingContext">Message used during the step execution.</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<StepResult> ExecuteAsync(MessagingContext messagingContext, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
