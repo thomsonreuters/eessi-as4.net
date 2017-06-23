a67a9bb65eb9a41fdccd37f6739c157f3151d888
Stijn Moreels <stijn.moreels@codit.eu>
2017-06-23 11:52:53 +0200

    Remove overkill-tests

--- source/AS4/Eu.EDelivery.AS4.UnitTests/Agents/GivenAgentProviderFacts.cs
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Agents/GivenAgentProviderFacts.cs
@@ -29,47 +29,6 @@ namespace Eu.EDelivery.AS4.UnitTests.Agents
         }
 
         [Fact]
-        public void AssemblesAgents()
-        {
-            // Arrange
-            var sut = new AgentProvider(new SingleAgentConfig());
-
-            // Act
-            IEnumerable<IAgent> agents = sut.GetAgents();
-
-            // Assert
-            Assert.Equal(1, agents.Count());
-            IAgent singleAgent = agents.First();
-
-            Assert.Equal(SingleAgentConfig.ExpectedSteps, GetsActualSteps(singleAgent));
-            Assert.Equal(SingleAgentConfig.TransformerConfig, GetsActualTransformer(singleAgent));
-            Assert.IsType<StubReceiver>(GetsActualReceiver(singleAgent));
-        }
-
-        private static AS4.Model.Internal.Steps GetsActualSteps(IAgent agent)
-        {
-            return GetField<AS4.Model.Internal.Steps>(agent, "_stepConfiguration");
-        }
-
-        private static Transformer GetsActualTransformer(IAgent agent)
-        {
-            return GetField<Transformer>(agent, "_transformerConfiguration");
-        }
-
-        private static IReceiver GetsActualReceiver(IAgent agent)
-        {
-            return GetField<IReceiver>(agent, "_receiver");
-        }
-
-        private static T GetField<T>(IAgent agent, string field) where T : class
-        {
-            FieldInfo stepConfig = agent.GetType().GetField(field, BindingFlags.NonPublic | BindingFlags.Instance);
-            Assert.NotNull(stepConfig);
-
-            return stepConfig.GetValue(agent) as T;
-        }
-
-        [Fact]
         public void AssembleAgentBaseClasses_IfTypeIsSpecified()
         {
             // Arrange
