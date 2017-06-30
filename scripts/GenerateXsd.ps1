71d4c5d445990644318138516825a1bdb79bcb75
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-06-30 20:12:05 +0200

    Modified sample pmodes to be conform to the xsd

--- scripts/GenerateXsd.ps1
+++ scripts/GenerateXsd.ps1
@@ -1,24 +1,25 @@
+param([string]$binDirectory = "../output/Staging/bin", [string]$outputDirectory = "../output/Staging/Documentation/Schemas")
 
 $cmd = "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\xsd.exe"
 
 # Export new XSD files from the build assembly
-& $cmd ../output/Staging/bin/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.Submit.SubmitMessage /o:../output/Staging/Documentation/Schemas
-Rename-Item ../output/Staging/Documentation/Schemas/schema0.xsd submitmessage-schema.xsd
+& $cmd $binDirectory/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.Submit.SubmitMessage /o:$outputDirectory
+Move-Item $outputDirectory/schema0.xsd submitmessage-schema.xsd -Force
 
-& $cmd ../output/Staging/bin/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.Deliver.DeliverMessage /o:../output/Staging/Documentation/Schemas
-Rename-Item ../output/Staging/Documentation/Schemas/schema0.xsd delivermessage-schema.xsd
+& $cmd $binDirectory/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.Deliver.DeliverMessage /o:$outputDirectory
+Move-Item $outputDirectory/schema0.xsd delivermessage-schema.xsd -Force
 
-& $cmd ../output/Staging/bin/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.Notify.NotifyMessage /o:../output/Staging/Documentation/Schemas
-Rename-Item ../output/Staging/Documentation/Schemas/schema0.xsd notifymessage-schema.xsd
+& $cmd $binDirectory/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.Notify.NotifyMessage /o:$outputDirectory
+Move-Item $outputDirectory/schema0.xsd notifymessage-schema.xsd -Force
 
-& $cmd ../output/Staging/bin/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.PMode.SendingProcessingMode /o:../output/Staging/Documentation/Schemas
-Rename-Item ../output/Staging/Documentation/Schemas/schema0.xsd send-pmode-schema.xsd
+& $cmd $binDirectory/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.PMode.SendingProcessingMode /o:$outputDirectory
+Move-Item $outputDirectory/schema0.xsd send-pmode-schema.xsd -Force
 
-& $cmd ../output/Staging/bin/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.PMode.ReceivingProcessingMode /o:../output/Staging/Documentation/Schemas
-Rename-Item ../output/Staging/Documentation/Schemas/schema0.xsd receive-pmode-schema.xsd
+& $cmd $binDirectory/Eu.EDelivery.AS4.dll  /type:Eu.EDelivery.AS4.Model.PMode.ReceivingProcessingMode /o:$outputDirectory
+Move-Item $outputDirectory/schema0.xsd receive-pmode-schema.xsd -Force
 
 # Update each 'any' element with the 'processContents' attribute set to 'lax'
-Get-ChildItem ../output/Staging/Documentation/Schemas -Filter '*.xsd' | % {
+Get-ChildItem $outputDirectory -Filter '*.xsd' | % {
         $content = [xml](Get-Content $_.FullName)
      
         Select-Xml $content -XPath "//*[local-name()='any']" | % {
@@ -30,8 +31,8 @@ Get-ChildItem ../output/Staging/Documentation/Schemas -Filter '*.xsd' | % {
     }
 
 # Copy the new exported schemas to the current documentation folder
-Copy-Item -Path ../output/Staging/Documentation/Schemas/submitmessage-schema.xsd -Destination ../output/doc/schemas/submitmessage-schema.xsd -Force
-Copy-Item -Path ../output/Staging/Documentation/Schemas/delivermessage-schema.xsd -Destination ../output/doc/schemas/delivermessage-schema.xsd -Force
-Copy-Item -Path ../output/Staging/Documentation/Schemas/notifymessage-schema.xsd -Destination ../output/doc/schemas/notifymessage-schema.xsd -Force
-Copy-Item -Path ../output/Staging/Documentation/Schemas/send-pmode-schema.xsd -Destination ../output/doc/schemas/send-pmode-schema.xsd -Force
-Copy-Item -Path ../output/Staging/Documentation/Schemas/receive-pmode-schema.xsd -Destination ../output/doc/schemas/receive-pmode-schema.xsd -Force
+Copy-Item -Path $outputDirectory/submitmessage-schema.xsd -Destination ../output/doc/schemas/submitmessage-schema.xsd -Force
+Copy-Item -Path $outputDirectory/delivermessage-schema.xsd -Destination ../output/doc/schemas/delivermessage-schema.xsd -Force
+Copy-Item -Path $outputDirectory/notifymessage-schema.xsd -Destination ../output/doc/schemas/notifymessage-schema.xsd -Force
+Copy-Item -Path $outputDirectory/send-pmode-schema.xsd -Destination ../output/doc/schemas/send-pmode-schema.xsd -Force
+Copy-Item -Path $outputDirectory/receive-pmode-schema.xsd -Destination ../output/doc/schemas/receive-pmode-schema.xsd -Force
