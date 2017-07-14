d07c0a6dfe8c03d65cbd99aa64f93b55820dcbef
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-07-14 16:59:50 +0200

    PollingInterval on FileReceiver should be configurable in Fe.

--- source/Receivers/Eu.EDelivery.AS4.Receivers/FileReceiver.cs
+++ source/Receivers/Eu.EDelivery.AS4.Receivers/FileReceiver.cs
@@ -24,7 +24,7 @@ namespace Eu.EDelivery.AS4.Receivers
         private readonly SynchronizedCollection<FileInfo> _pendingFiles = new SynchronizedCollection<FileInfo>();
         private readonly IMimeTypeRepository _repository;
 
-        private IDictionary<string, string> _properties;
+        private IDictionary<string, string> _properties;        
 
         /// <summary>
         /// Initializes a new instance of the <see cref="FileReceiver" /> class
@@ -45,10 +45,13 @@ namespace Eu.EDelivery.AS4.Receivers
         [Info("Maximum number of concurrent processed files")]
         private int BatchSize { get; set; }
 
+        [Info("Interval to poll on the configured file-path")]
+        private TimeSpan _pollingInterval;
+
         protected override ILogger Logger { get; }
 
         [Info("Polling interval", "", "int")]
-        protected override TimeSpan PollingInterval => FromProperties();
+        protected override TimeSpan PollingInterval => _pollingInterval;
 
         #region Configuration
 
@@ -76,6 +79,8 @@ namespace Eu.EDelivery.AS4.Receivers
             }
 
             BatchSize = batchSize;
+
+            _pollingInterval = ReadPollingIntervalFromProperties();
         }
 
         #endregion
@@ -242,7 +247,7 @@ namespace Eu.EDelivery.AS4.Receivers
             return filename;
         }
 
-        private TimeSpan FromProperties()
+        private TimeSpan ReadPollingIntervalFromProperties()
         {
             TimeSpan defaultInterval = TimeSpan.FromSeconds(3);
 
