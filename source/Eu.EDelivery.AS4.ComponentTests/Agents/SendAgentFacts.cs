0b3d2edc18db6f55f495e7e0c86e3e069e9ff0d7
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-08-17 16:55:14 +0200

    updated nuget packages

---
+++ source/Eu.EDelivery.AS4.ComponentTests/Agents/SendAgentFacts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eu.EDelivery.AS4.ComponentTests.Common;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.ComponentTests.Agents
+{
+    public class SendAgentFacts : ComponentTestTemplate
+    {
+        private readonly AS4Component _as4Msh;
+
+        public SendAgentFacts()
+        {
+            OverrideSettings("sendagent_http_settings.xml");
+            _as4Msh = AS4Component.Start(Environment.CurrentDirectory);
+        }
+
+        [Fact]
+        public void WhenIntermediary_ThenForwardReceivedSignalMessage()
+        {
+            
+        }
+    }
+}
