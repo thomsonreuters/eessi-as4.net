a94269763919f3fa4dd1a2e01e2db85c79ed23bf
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-08-25 16:02:43 +0200

    Start holodeck processes in another way.
    Related Work Items: #6784

--- source/ServiceHandler/Eu.EDelivery.AS4.IntegrationTests/Fixture/HolodeckFixture.cs
+++ source/ServiceHandler/Eu.EDelivery.AS4.IntegrationTests/Fixture/HolodeckFixture.cs
@@ -30,14 +30,36 @@ namespace Eu.EDelivery.AS4.IntegrationTests.Fixture
 
             service.RemoveDirectory(holodeck_A_db_path);
             service.RemoveDirectory(holodeck_B_db_path);
-            
-            Process holodeckA = Process.Start(@"C:\Program Files\Java\holodeck\holodeck-b2b-A\bin\startServer.bat");
-            Process holodeckB = Process.Start(@"C:\Program Files\Java\holodeck\holodeck-b2b-B\bin\startServer.bat");
+
+            Process holodeckA = StartHolodeck(@"C:\Program Files\Java\holodeck\holodeck-b2b-A\bin\startServer.bat");
+            Process holodeckB = StartHolodeck(@"C:\Program Files\Java\holodeck\holodeck-b2b-B\bin\startServer.bat");
 
             _parentProcess = new ParentProcess(holodeckA, holodeckB);
 
             // Make sure the Holodeck MSH's are started before continuing.
             System.Threading.Thread.Sleep(6000);
+        }
+
+        private static Process StartHolodeck(string executablePath)
+        {
+            Process p = new Process();
+
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(executablePath);
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            
+            p.ErrorDataReceived += delegate (object sender, DataReceivedEventArgs args)
+            {
+                Console.WriteLine(args.Data);
+            };
+
+            if (p.Start() == false)
+            {
+                throw new InvalidOperationException($"Unable to start holodeck. Exitcode =  {p.ExitCode}");
+            }
+
+            return p;
         }
 
         /// <summary>
