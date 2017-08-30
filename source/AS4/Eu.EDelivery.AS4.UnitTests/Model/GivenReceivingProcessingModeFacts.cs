7412a865e40b5fe31709a18b26d98bfc4938e6dd
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-08-30 12:34:44 +0200

    Refactored ReceivingPModeTests

--- source/AS4/Eu.EDelivery.AS4.UnitTests/Model/GivenReceivingProcessingModeFacts.cs
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Model/GivenReceivingProcessingModeFacts.cs
@@ -7,9 +7,7 @@ namespace Eu.EDelivery.AS4.UnitTests.Model
     /// Testing the <see cref="ReceivingProcessingMode" />
     /// </summary>
     public class GivenReceivingProcessingModeFacts
-    {
-        private ReceivingProcessingMode _pmode;
-
+    {        
         /// <summary>
         /// Testing the PMode Model for its defaults
         /// </summary>
@@ -19,82 +17,82 @@ namespace Eu.EDelivery.AS4.UnitTests.Model
             public void ThenDecryptionIsDefault()
             {
                 // Act
-                _pmode = new ReceivingProcessingMode();
+                var pmode = new ReceivingProcessingMode();
 
                 // Assert
-                Assert.NotNull(_pmode.Security);
-                Assert.NotNull(_pmode.Security.Decryption);
-                Assert.Equal(Limit.Allowed, _pmode.Security.Decryption.Encryption);
+                Assert.NotNull(pmode.Security);
+                Assert.NotNull(pmode.Security.Decryption);
+                Assert.Equal(Limit.Allowed, pmode.Security.Decryption.Encryption);
             }
 
             [Fact]
             public void ThenDeliverIsDefault()
             {
                 // Act
-                _pmode = new ReceivingProcessingMode();
+                var pmode = new ReceivingProcessingMode();
 
                 // Assert
-                Assert.NotNull(_pmode.MessageHandling.DeliverInformation);
-                Assert.False(_pmode.MessageHandling.DeliverInformation.IsEnabled);
+                Assert.NotNull(pmode.MessageHandling.DeliverInformation);
+                Assert.False(pmode.MessageHandling.DeliverInformation.IsEnabled);
             }
 
             [Fact]
             public void ThenDuplicateDetectionIsFalse()
             {
                 // Act
-                _pmode = new ReceivingProcessingMode();
+                var pmode = new ReceivingProcessingMode();
 
                 // Assert
-                Assert.NotNull(_pmode.Reliability);
-                Assert.NotNull(_pmode.Reliability.DuplicateElimination);
-                Assert.False(_pmode.Reliability.DuplicateElimination.IsEnabled);
+                Assert.NotNull(pmode.Reliability);
+                Assert.NotNull(pmode.Reliability.DuplicateElimination);
+                Assert.False(pmode.Reliability.DuplicateElimination.IsEnabled);
             }
 
             [Fact]
             public void ThenErrorHandlingIsDefault()
             {
                 // Act
-                _pmode = new ReceivingProcessingMode();
+                var pmode = new ReceivingProcessingMode();
 
                 // Assert
-                Assert.NotNull(_pmode.ReplyHandling.ErrorHandling);
-                Assert.False(_pmode.ReplyHandling.ErrorHandling.UseSoapFault);
+                Assert.NotNull(pmode.ReplyHandling.ErrorHandling);
+                Assert.False(pmode.ReplyHandling.ErrorHandling.UseSoapFault);
             }
 
             [Fact]
             public void ThenExceptionHandlingIsDefault()
             {
                 // Act
-                _pmode = new ReceivingProcessingMode();
+                var pmode = new ReceivingProcessingMode();
 
                 // Assert
-                Assert.NotNull(_pmode.ExceptionHandling);
-                Assert.NotNull(_pmode.ExceptionHandling.NotifyMethod);
-                Assert.False(_pmode.ExceptionHandling.NotifyMessageConsumer);
+                Assert.NotNull(pmode.ExceptionHandling);
+                Assert.NotNull(pmode.ExceptionHandling.NotifyMethod);
+                Assert.False(pmode.ExceptionHandling.NotifyMessageConsumer);
             }
 
             [Fact]
             public void ThenReceiptHandlingIsDefault()
             {
                 // Act
-                _pmode = new ReceivingProcessingMode();
+                var pmode = new ReceivingProcessingMode();
 
                 // Assert
-                Assert.NotNull(_pmode.ReplyHandling.ReceiptHandling);
-                Assert.False(_pmode.ReplyHandling.ReceiptHandling.UseNNRFormat);
-                Assert.Equal(ReplyPattern.Response, _pmode.ReplyHandling.ReplyPattern);
+                Assert.NotNull(pmode.ReplyHandling.ReceiptHandling);
+                Assert.False(pmode.ReplyHandling.ReceiptHandling.UseNNRFormat);
+                Assert.Equal(ReplyPattern.Response, pmode.ReplyHandling.ReplyPattern);
             }
 
             [Fact]
             public void ThenSigningVerificationIsDefault()
             {
                 // Act
-                _pmode = new ReceivingProcessingMode();
+                var pmode = new ReceivingProcessingMode();
 
                 // Assert
-                Assert.NotNull(_pmode.Security);
-                Assert.NotNull(_pmode.Security.SigningVerification);
-                Assert.Equal(Limit.Allowed, _pmode.Security.SigningVerification.Signature);
+                Assert.NotNull(pmode.Security);
+                Assert.NotNull(pmode.Security.SigningVerification);
+                Assert.Equal(Limit.Allowed, pmode.Security.SigningVerification.Signature);
             }
         }
     }
