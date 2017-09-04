18f24551a58b5f80c704556f78f48626d1edf36b
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-09-04 09:51:36 +0200

    Refactored Minder specific Agent creation outside the AgentProvider class

--- source/ServiceHandler/Eu.EDelivery.AS4.ServiceHandler/Agents/AgentProvider.cs
+++ source/ServiceHandler/Eu.EDelivery.AS4.ServiceHandler/Agents/AgentProvider.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using Eu.EDelivery.AS4.Agents;
 using Eu.EDelivery.AS4.Common;
 using Eu.EDelivery.AS4.Exceptions.Handlers;
-using Eu.EDelivery.AS4.Model.Internal;
 using Eu.EDelivery.AS4.Receivers;
 using Eu.EDelivery.AS4.ServiceHandler.Builder;
-using Eu.EDelivery.AS4.Steps.Receive;
-using Eu.EDelivery.AS4.Steps.Send;
-using Eu.EDelivery.AS4.Steps.Submit;
 using NLog;
 
 namespace Eu.EDelivery.AS4.ServiceHandler.Agents
@@ -22,7 +17,7 @@ namespace Eu.EDelivery.AS4.ServiceHandler.Agents
     public class AgentProvider
     {
         private readonly IConfig _config;
-        private readonly ICollection<IAgent> _agents;
+        private readonly List<IAgent> _agents;
         private readonly ILogger _logger;
 
         /// <summary>
@@ -33,7 +28,7 @@ namespace Eu.EDelivery.AS4.ServiceHandler.Agents
         {
             _config = config;
             _logger = LogManager.GetCurrentClassLogger();
-            _agents = new Collection<IAgent>();
+            _agents = new List<IAgent>();
 
             TryAddCustomAgentsToProvider();
         }
@@ -44,7 +39,7 @@ namespace Eu.EDelivery.AS4.ServiceHandler.Agents
         /// <returns></returns>
         public IEnumerable<IAgent> GetAgents()
         {
-            return _agents;
+            return _agents.ToArray();
         }
 
         private void TryAddCustomAgentsToProvider()
@@ -83,86 +78,9 @@ namespace Eu.EDelivery.AS4.ServiceHandler.Agents
         [ExcludeFromCodeCoverage]
         private void AddMinderAgentsToProvider()
         {
-            IEnumerable<SettingsMinderAgent> minderTestAgents = _config.GetEnabledMinderTestAgents();
+            var minderTestAgents = MinderAgentProvider.GetMinderSpecificAgentsFromConfig(_config);
 
-            foreach (SettingsMinderAgent agent in minderTestAgents)
-            {
-                _agents.Add(CreateMinderTestAgent(agent.Url, agent.UseLogging, agent.Transformer));
-            }
-        }
-
-        [ExcludeFromCodeCoverage]
-        private static Agent CreateMinderTestAgent(string url, bool useLogging, Transformer transformerConfig)
-        {
-            var receiver = new HttpReceiver();
-
-            receiver.Configure(new[] { new Setting("Url", url), new Setting("UseLogging", useLogging.ToString()) });
-
-            return new Agent(
-                "Minder Submit/Receive Agent",
-                receiver,
-                transformerConfig,
-                new MinderExceptionHandler(), 
-                (CreateMinderHappyFlow(), CreateReceiveUnhappyFlow()));
-        }
-
-        [ExcludeFromCodeCoverage]
-        private static ConditionalStepConfig CreateMinderHappyFlow()
-        {
-            Func<MessagingContext, bool> isSubmitMessage = m => m.Mode == MessagingContextMode.Submit;                
-
-            Step[] submitStepConfig = CreateSubmitSteps();
-            Step[] receiveStepConfig = CreateReceiveHappyFlowSteps();
-
-            return new ConditionalStepConfig(isSubmitMessage, submitStepConfig, receiveStepConfig);
-        }
-
-        private static ConditionalStepConfig CreateReceiveUnhappyFlow()
-        {
-            Func<MessagingContext, bool> isSubmitMessage = m => m.Mode == MessagingContextMode.Submit;
-
-            var submitStepConfig = new Step[0];
-            Step[] receiveStepConfig = CreateReceiveUnhappyFlowSteps();
-
-            return new ConditionalStepConfig(isSubmitMessage, submitStepConfig, receiveStepConfig);
-        }
-
-        [ExcludeFromCodeCoverage]
-        private static Step[] CreateSubmitSteps()
-        {
-            return new[]
-            {
-                new Step {Type = typeof(StoreAS4MessageStep).AssemblyQualifiedName},
-                new Step {Type = typeof(CreateAS4ReceiptStep).AssemblyQualifiedName}
-            };
-        }
-
-        [ExcludeFromCodeCoverage]
-        private static Step[] CreateReceiveHappyFlowSteps()
-        {
-            return new[]
-            {
-                new Step {Type = typeof(SaveReceivedMessageStep).AssemblyQualifiedName},
-                new Step {Type = typeof(DeterminePModesStep).AssemblyQualifiedName},
-                new Step {Type = typeof(ValidateAS4MessageStep).AssemblyQualifiedName},
-                new Step {Type = typeof(DecryptAS4MessageStep).AssemblyQualifiedName},
-                new Step {Type = typeof(VerifySignatureAS4MessageStep).AssemblyQualifiedName},                
-                new Step {Type = typeof(DecompressAttachmentsStep).AssemblyQualifiedName},
-                new Step {Type = typeof(UpdateReceivedAS4MessageBodyStep).AssemblyQualifiedName},
-                new Step {Type = typeof(CreateAS4ReceiptStep).AssemblyQualifiedName},                
-                new Step {Type = typeof(SignAS4MessageStep).AssemblyQualifiedName},
-                new Step {Type = typeof(SendAS4SignalMessageStep).AssemblyQualifiedName},
-            };
-        }
-
-        private static Step[] CreateReceiveUnhappyFlowSteps()
-        {
-            return new[]
-            {
-                new Step {Type = typeof(CreateAS4ErrorStep).AssemblyQualifiedName},
-                new Step {Type = typeof(SignAS4MessageStep).AssemblyQualifiedName},
-                new Step {Type = typeof(SendAS4SignalMessageStep).AssemblyQualifiedName}
-            };
+            _agents.AddRange(minderTestAgents);
         }
     }
 }
