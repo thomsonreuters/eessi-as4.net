1292956988e9d8384f925fb884b077fe92aa2393
Jan Tilburgh <jan.tilburgh@codit.eu>
2017-09-11 11:11:22 +0000

    Merged PR 1293: Remove assets folder
    Remove assets folder

--- source/Fe/Eu.EDelivery.AS4.Fe/ui/config/webpack.common.js
+++ source/Fe/Eu.EDelivery.AS4.Fe/ui/config/webpack.common.js
@@ -241,8 +241,7 @@ module.exports = function (options) {
        * See: https://www.npmjs.com/package/copy-webpack-plugin
        */
       new CopyWebpackPlugin([
-        { from: 'src/assets', to: 'assets' },
-        { from: 'src/meta'}
+        { from: 'src/assets', to: 'assets' }
       ]),
 
 
