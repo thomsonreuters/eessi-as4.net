9c779ecc5cb0d56cf402db3c023b29a6d748b9e1
Jan Tilburgh <jan.tilburgh@codit.eu>
2017-09-12 09:01:31 +0000

    Merged PR 1295: Sanitize inputs like 01:1:00 to 01:01:00 or 00:00:1 to 00:00:01
    Sanitize inputs like 01:1:00 to 01:01:00 or 00:00:1 to 00:00:01

--- source/Fe/Eu.EDelivery.AS4.Fe/ui/src/app/runtime/runtimesetting/runtimesetting.component.ts
+++ source/Fe/Eu.EDelivery.AS4.Fe/ui/src/app/runtime/runtimesetting/runtimesetting.component.ts
@@ -17,11 +17,42 @@ export class RuntimeSettingComponent {
     @Input() public controlSize: string;
     @Output() public onChange = new EventEmitter<Setting>();
     public timeSpanMask = [/[0-9]/, /[0-9]/, ':', /[0-5]/, /[0-9]/, ':', /[0-5]/, /[0-9]/];
+    private _regex = new RegExp(/[0-9]_/g);
+    private _initial = true;
     public hasParameters(): boolean {
         let array = (<FormArray>this.control.get('attributes')!).controls;
         if (!!!array) {
             return false;
         }
         return array.length > 0;
+    }
+    public pipe = (input: string) => {
+        if (!this._initial) {
+            return input;
+        }
+        this._initial = false;
+        let result = '';
+        if (!!!input) {
+            return input;
+        }
+        input
+            .split(':')
+            .forEach((splitted) => {
+                // Check if the splitted value contains <number>_
+                if (splitted === '__') {
+                    result = this.append(result, '00');
+                } else if (this._regex.test(splitted)) {
+                    result = this.append(result, '0' + splitted[0]);
+                } else {
+                    result = this.append(result, splitted);
+                }
+            });
+        return result;
+    }
+    private append(input: string, append: string): string {
+        if (!!!input) {
+            return append;
+        }
+        return `${input}:${append}`;
     }
 }
