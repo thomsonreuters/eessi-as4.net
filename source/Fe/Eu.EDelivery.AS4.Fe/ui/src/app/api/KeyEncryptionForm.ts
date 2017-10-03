9c9abf42c61ceb2bdc8887d4a23e8560a19af229
Jan Tilburgh <jan.tilburgh@codit.eu>
2017-10-03 15:14:32 +0000

    Merged PR 1497: Merge fe-jwt-setup-reload to master

--- source/Fe/Eu.EDelivery.AS4.Fe/ui/src/app/api/KeyEncryptionForm.ts
+++ source/Fe/Eu.EDelivery.AS4.Fe/ui/src/app/api/KeyEncryptionForm.ts
@@ -5,17 +5,13 @@ import { ItemType } from './ItemType';
 import { FormWrapper } from './../common/form.service';
 
 export class KeyEncryptionForm {
-    // public static defaultTransportAlgorithm: string = 'http://www.w3.org/2009/xmlenc11#rsa-oaep';
-    // public static defaultDigestAlgorithm: string = 'http://www.w3.org/2000/09/xmldsig#sha1';
-    // public static transportAlgorithm: string = 'http://www.w3.org/2009/xmlenc11#rsa-oaep';
-    // public static defaultMgfAlgorithm: string = 'http://www.w3.org/2009/xmlenc11#mgf1sha1';
     private static disabledAlgo = 'http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p';
     public static getForm(formBuilder: FormWrapper, current: KeyEncryption, path: string, runtime: ItemType[]): FormWrapper {
         let form = formBuilder
             .group({
                 [KeyEncryption.FIELD_transportAlgorithm]: [formBuilder.createFieldValue(current, KeyEncryption.FIELD_transportAlgorithm, path, null, runtime), Validators.required],
                 [KeyEncryption.FIELD_digestAlgorithm]: [formBuilder.createFieldValue(current, KeyEncryption.FIELD_digestAlgorithm, path, null, runtime), Validators.required],
-                [KeyEncryption.FIELD_mgfAlgorithm]: [formBuilder.createFieldValue(current, KeyEncryption.FIELD_mgfAlgorithm, path, null, runtime), Validators.required]
+                [KeyEncryption.FIELD_mgfAlgorithm]: [formBuilder.createFieldValue(current, KeyEncryption.FIELD_mgfAlgorithm, path, null, runtime)]
             })
             .onChange<string>(KeyEncryption.FIELD_transportAlgorithm, (value, wrapper) => {
                 let mgf = wrapper.form.get(KeyEncryption.FIELD_mgfAlgorithm)!;
