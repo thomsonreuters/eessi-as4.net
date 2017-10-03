8c3061d73013e03c94779dabec5e4aad4d756bca
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-10-03 12:01:25 +0200

    Added unit-test to verify if (de)serialization of default values for pmode went well.

---
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Model/PMode/GivenSendingPModeFacts.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Eu.EDelivery.AS4.Model.PMode;
+using Eu.EDelivery.AS4.Serialization;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.UnitTests.Model.PMode
+{
+    public class GivenSendingPModeFacts
+    {
+        [Fact]
+        public async Task DeserializedPModeWithDefaultValuesContainsDefaultValues()
+        {
+            var sendingPMode = new SendingProcessingMode();
+
+            var serialized = await AS4XmlSerializer.ToStringAsync(sendingPMode);
+
+            var deserialized = await AS4XmlSerializer.FromStringAsync<SendingProcessingMode>(serialized);
+
+            Assert.Equal(sendingPMode.Security.Encryption.Algorithm, deserialized.Security.Encryption.Algorithm);
+            Assert.Equal(sendingPMode.Security.Encryption.AlgorithmKeySize, deserialized.Security.Encryption.AlgorithmKeySize);
+        }
+    }
+}
