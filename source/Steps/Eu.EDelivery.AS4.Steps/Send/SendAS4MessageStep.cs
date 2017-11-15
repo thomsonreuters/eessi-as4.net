fca7066826b763e580b2465a1c77eca76607cbc3
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-11-15 17:13:57 +0100

    Always set content-length if possible.

--- source/Steps/Eu.EDelivery.AS4.Steps/Send/SendAS4MessageStep.cs
+++ source/Steps/Eu.EDelivery.AS4.Steps/Send/SendAS4MessageStep.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -177,7 +178,13 @@ namespace Eu.EDelivery.AS4.Steps.Send
                 {
                     if (messagingContext.ReceivedMessage != null)
                     {
-                        await messagingContext.ReceivedMessage.UnderlyingStream.CopyToFastAsync(requestStream).ConfigureAwait(false);
+                        var sw = new Stopwatch();
+                        sw.Start();
+
+                        await messagingContext.ReceivedMessage.UnderlyingStream.CopyToFastAsync(requestStream, 8192).ConfigureAwait(false);
+
+                        sw.Stop();
+                        Logger.Trace($"Writing to request stream took {sw.ElapsedMilliseconds} milliseconds");
                     }
                     else
                     {
@@ -210,13 +217,13 @@ namespace Eu.EDelivery.AS4.Steps.Send
         private static void SetAdditionalRequestHeaders(HttpWebRequest request, MessagingContext messagingContext)
         {
             long messageSize = TryGetMessageSize(messagingContext);
-            const int threshold = 209_715_200;
 
-            if (messageSize > threshold)
+            if (messageSize >= 0)
             {
-                request.AllowWriteStreamBuffering = false;
                 request.ContentLength = messageSize;
             }
+
+            request.AllowWriteStreamBuffering = false;           
         }
 
         private static async Task<AS4Message> GetAS4MessageFromContextAsync(MessagingContext context)
