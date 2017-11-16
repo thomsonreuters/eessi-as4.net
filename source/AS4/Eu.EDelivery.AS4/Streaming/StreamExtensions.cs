396c09af9c84db9cc131abfccca3f544a7af37ba
Frederik Gheysels <frederik.gheysels@codit.eu>
2017-11-16 15:19:58 +0100

    Improved CopyToFastAsync

--- source/AS4/Eu.EDelivery.AS4/Streaming/StreamExtensions.cs
+++ source/AS4/Eu.EDelivery.AS4/Streaming/StreamExtensions.cs
@@ -27,35 +27,35 @@ namespace Eu.EDelivery.AS4.Streaming
             return new byte[0];
         }
 
-        public static async Task CopyToFastAsync(this Stream source, Stream target)
-        {
-            await CopyToFastAsync(source, target, 81_920);
-        }
+        public const int DefaultCopyToFastBufferSize = 81_920;
 
         /// <summary>
         /// Copies the source stream to the target stream asynchronously
         /// </summary>
         /// <param name="source"></param>
         /// <param name="target"></param>
-        /// <param name="bufferSize">The size of the buffer that must be applied when reading or writing during copy.</param>
         /// <remarks>Reading from the source-stream and writing to the target stream occurs in parallel.</remarks>
         /// <returns></returns>
-        public static async Task CopyToFastAsync(this Stream source, Stream target, int bufferSize)
+        public static async Task CopyToFastAsync(this Stream source, Stream target)
         {
+            // Set the length of the targetstream upfront if we can do that.
+            // This can improve performance significantly.
+            if (source.CanSeek && target.CanSeek)
+            {
+                target.SetLength(target.Length + (source.Length - source.Position));
+            }
+
 #if CONCURRENT_IO
 
-            // We have a buffer of bufferSize.
-            // The ioCount constant is exactly the half of bufferSize.
-            // We're reading ioCount bytes from the source-stream inside one half of the buffer
+            // We're reading bufferSize bytes from the source-stream inside one half of the buffer
             // while the writeTask is writing the other half of the buffer to the target-stream.
-
+            int bufferSize = DetermineOptimalBufferSize(source);
             int ioBufferSize = bufferSize * 2;
-            int actionBufferSize = bufferSize;
 
             byte[] buffer = new byte[ioBufferSize];
             int curoff = 0;
 
-            Task<int> readTask = source.ReadAsync(buffer, curoff, actionBufferSize);
+            Task<int> readTask = source.ReadAsync(buffer, curoff, bufferSize);
             Task writeTask = Task.CompletedTask;
             int len;
 
@@ -64,14 +64,36 @@ namespace Eu.EDelivery.AS4.Streaming
                 await writeTask.ConfigureAwait(false);
                 writeTask = target.WriteAsync(buffer, curoff, len);
 
-                curoff ^= actionBufferSize;
-                readTask = source.ReadAsync(buffer, curoff, actionBufferSize);
+                curoff ^= bufferSize;
+                readTask = source.ReadAsync(buffer, curoff, bufferSize);
             }
 
             await writeTask.ConfigureAwait(false);
 #else
             await source.CopyToAsync(target).ConfigureAwait(false);
 #endif
+        }
+
+        private static int DetermineOptimalBufferSize(Stream sourceStream)
+        {
+            if (sourceStream.CanSeek == false)
+            {
+                return DefaultCopyToFastBufferSize;
+            }
+
+            if (sourceStream.Length < DefaultCopyToFastBufferSize)
+            {
+                int factor = (int)sourceStream.Length / (4096 * 2);
+
+                if (factor == 0)
+                {
+                    return 4096;
+                }
+
+                return 4096 * factor;
+            }
+
+            return DefaultCopyToFastBufferSize;
         }
     }
 }
