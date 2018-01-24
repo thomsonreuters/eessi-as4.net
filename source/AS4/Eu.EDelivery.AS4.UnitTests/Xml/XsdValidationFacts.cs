5f945f7733ce46e439787872c933426afc55c3de
Stijn Moreels <stijn.moreels@codit.eu>
2018-01-24 09:13:27 +0100

    Place same XSD tests in the same Test Collection
    This way all the XSD-related tests are not running in parallel.

--- source/AS4/Eu.EDelivery.AS4.UnitTests/Xml/XsdValidationFacts.cs
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Xml/XsdValidationFacts.cs
@@ -11,6 +11,7 @@ namespace Eu.EDelivery.AS4.UnitTests.Xml
 {
     public class XsdValidationFacts : IClassFixture<XsdValidationFixture>
     {
+        [Collection("XSD")]
         public class SamplesXsdValidationFacts : XsdValidationFacts
         {
             [Fact]
@@ -53,6 +54,7 @@ namespace Eu.EDelivery.AS4.UnitTests.Xml
             }
         }
 
+        [Collection("XSD")]
         public class ConformanceTestXsdValidationFacts : XsdValidationFacts
         {
             [Fact]
@@ -70,6 +72,7 @@ namespace Eu.EDelivery.AS4.UnitTests.Xml
             }
         }
 
+        [Collection("XSD")]
         public class EessiConformanceXsdValidationFacts : XsdValidationFacts
         {
             [Fact]
