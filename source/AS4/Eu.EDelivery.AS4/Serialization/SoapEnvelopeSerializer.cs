535baa226435649aa44a091995d565e94acdcacf
Frederik Gheysels <frederik.gheysels@codit.eu>
2018-01-30 15:24:36 +0100

    FIX: throw InvalidMessageException when stream cannot be deserialized to AS4Message.

--- source/AS4/Eu.EDelivery.AS4/Serialization/SoapEnvelopeSerializer.cs
+++ source/AS4/Eu.EDelivery.AS4/Serialization/SoapEnvelopeSerializer.cs
@@ -8,6 +8,7 @@ using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 using Eu.EDelivery.AS4.Builders.Security;
+using Eu.EDelivery.AS4.Exceptions;
 using Eu.EDelivery.AS4.Model.Core;
 using Eu.EDelivery.AS4.Resources;
 using Eu.EDelivery.AS4.Security.Strategies;
@@ -144,7 +145,6 @@ namespace Eu.EDelivery.AS4.Serialization
                 throw new ArgumentNullException(nameof(envelopeStream));
             }
 
-
             XmlDocument envelopeDocument = LoadXmlDocument(envelopeStream);
 
             // Sometimes throws 'The 'http://www.w3.org/XML/1998/namespace:lang' attribute is not declared.'
@@ -155,6 +155,11 @@ namespace Eu.EDelivery.AS4.Serialization
             var securityHeader = DeserializeSecurityHeader(envelopeDocument, nsMgr);
             var messagingHeader = DeserializeMessagingHeader(envelopeDocument, nsMgr);
             var body = DeserializeBody(envelopeDocument, nsMgr);
+
+            if (messagingHeader == null)
+            {
+                throw new InvalidMessageException("The envelopeStream does not contain a Messaging element");
+            }
 
             AS4Message as4Message = AS4Message.Create(envelopeDocument, contentType, securityHeader, messagingHeader, body);
 
