6450afe3d59da70c7c38306045e8a897125c296f
Stijn Moreels <stijn.moreels@codit.eu>
2018-02-09 07:44:51 +0100

    Add Component Tests for included Samples in the Release Package
    By adding these tests, we are always sure that the samples we include in
    our Release Package work as expected.
    
     #6454

---
+++ source/Eu.EDelivery.AS4.ComponentTests/Samples/SampleTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.ComponentTests.Samples
+{
+    public class SampleTests : SampleTestTemplate
+    {
+        [Theory]
+        [InlineData("01-sample-message.xml", "earth.jpg", "*@*.xml")]
+        [InlineData("02-sample-message.xml", "earth.jpg", "xml-sample.xml", "*@*.xml")]
+        [InlineData("03-sample-message.xml", "earth.jpg", "xml-sample.xml", "*@*.xml")]
+        public void Sending_Sample_01_Result_In_Delivered_And_Notified_Files(string file, params string[] deliverMessages)
+        {
+            PutSample(file);
+
+            // Wait some time till component has processed the sample
+            Thread.Sleep(TimeSpan.FromSeconds(15));
+
+            Assert.All(deliverMessages, m => AssertFiles(@".\messages\in", m));
+            AssertFiles(@".\messages\receipts", "*@*.xml");
+        }
+
+        private static void AssertFiles(string path, string searchPattern)
+        {
+            Assert.NotEmpty(Directory.EnumerateFiles(path, searchPattern));
+        }
+    }
+}
