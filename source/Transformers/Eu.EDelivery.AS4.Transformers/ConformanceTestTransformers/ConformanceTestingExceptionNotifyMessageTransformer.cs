d42aec467045dea2a8608496695a59714628eaa2
Stijn Moreels <stijn.moreels@codit.eu>
2018-02-12 12:39:09 +0100

    Use a different base Transformer for the Exception Notification
    By using a different base Transformer for our Conformance Testing
    transformation, we can remove the obsolete Exception Transformer.
    
     #8385

--- source/Transformers/Eu.EDelivery.AS4.Transformers/ConformanceTestTransformers/ConformanceTestingExceptionNotifyMessageTransformer.cs
+++ source/Transformers/Eu.EDelivery.AS4.Transformers/ConformanceTestTransformers/ConformanceTestingExceptionNotifyMessageTransformer.cs
@@ -7,7 +7,7 @@ using Eu.EDelivery.AS4.Model.Notify;
 namespace Eu.EDelivery.AS4.Transformers.ConformanceTestTransformers
 {
     [ExcludeFromCodeCoverage]
-    public class ConformanceTestingExceptionNotifyMessageTransformer : ExceptionToNotifyMessageTransformer
+    public class ConformanceTestingExceptionNotifyMessageTransformer : NotifyMessageTransformer
     {
         protected override async Task<NotifyMessageEnvelope> CreateNotifyMessageEnvelope(AS4Message as4Message, Type receivedEntityType)
         {
