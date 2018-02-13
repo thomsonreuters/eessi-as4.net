3a92e489bf2185e17caf3f253eabcc6bec042dcb
Stijn Moreels <stijn.moreels@codit.eu>
2018-02-13 07:29:24 +0100

    PR-FIX: undo removal of obsolete Exception Transformer

---
+++ source/Transformers/Eu.EDelivery.AS4.Transformers/ExceptionToNotifyMessageTransformer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Eu.EDelivery.AS4.Builders.Core;
+using Eu.EDelivery.AS4.Common;
+using Eu.EDelivery.AS4.Entities;
+using Eu.EDelivery.AS4.Exceptions;
+using Eu.EDelivery.AS4.Model.Core;
+using Eu.EDelivery.AS4.Model.Internal;
+using Eu.EDelivery.AS4.Model.Notify;
+using Eu.EDelivery.AS4.Model.PMode;
+using Eu.EDelivery.AS4.Serialization;
+using NLog;
+
+namespace Eu.EDelivery.AS4.Transformers
+{
+    [Obsolete("Use the NotifyMessageTransformer instead")]
+    public class ExceptionToNotifyMessageTransformer : ITransformer
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Configures the <see cref="ITransformer"/> implementation with specific user-defined properties.
+        /// </summary>
+        /// <param name="properties">The properties.</param>
+        public void Configure(IDictionary<string, string> properties) { }
+
+        /// <summary>
+        /// Transform a given <see cref="ReceivedMessage" /> to a Canonical <see cref="MessagingContext" /> instance.
+        /// </summary>
+        /// <param name="message">Given message to transform.</param>
+        /// <param name="cancellationToken">Cancellation which stops the transforming.</param>
+        /// <returns></returns>
+        public async Task<MessagingContext> TransformAsync(ReceivedMessage message, CancellationToken cancellationToken)
+        {
+            ReceivedEntityMessage messageEntity = RetrieveEntityMessage(message);
+            ExceptionEntity exceptionEntity = RetrieveExceptionEntity(messageEntity);
+
+            AS4Message as4Message = await CreateErrorAS4Message(exceptionEntity, cancellationToken);
+
+            var messagingContext = new MessagingContext(await CreateNotifyMessageEnvelope(as4Message, exceptionEntity.GetType()))
+            {
+                SendingPMode = await GetPMode<SendingProcessingMode>(exceptionEntity.PMode),
+                ReceivingPMode = await GetPMode<ReceivingProcessingMode>(exceptionEntity.PMode)
+            };
+
+            Logger.Info($"[{exceptionEntity.EbmsRefToMessageId}] Exception AS4 Message is successfully transformed");
+
+            return messagingContext;
+        }
+
+        private static ReceivedEntityMessage RetrieveEntityMessage(ReceivedMessage message)
+        {
+            var entityMessage = message as ReceivedEntityMessage;
+            if (entityMessage == null)
+            {
+                throw new NotSupportedException($"Exception Transformer only supports '{nameof(ReceivedEntityMessage)}'");
+            }
+
+            return entityMessage;
+        }
+
+        private static ExceptionEntity RetrieveExceptionEntity(ReceivedEntityMessage messageEntity)
+        {
+            var exceptionEntity = messageEntity.Entity as ExceptionEntity;
+            if (exceptionEntity == null)
+            {
+                throw new NotSupportedException($"Exception Notify Transformer only supports '{nameof(ExceptionEntity)}'");
+            }
+
+            return exceptionEntity;
+        }
+
+        private static async Task<AS4Message> CreateErrorAS4Message(
+            ExceptionEntity exceptionEntity,
+            CancellationToken cancellationTokken)
+        {
+            Error error = CreateSignalErrorMessage(exceptionEntity);
+
+            AS4Message as4Message = AS4Message.Create(error, new SendingProcessingMode());
+
+            return as4Message;
+        }
+
+        private static Error CreateSignalErrorMessage(ExceptionEntity exceptionEntity)
+        {
+            var errorResult = new ErrorResult(exceptionEntity.Exception, ErrorAlias.Other);
+
+            return new ErrorBuilder()
+                .WithRefToEbmsMessageId(exceptionEntity.EbmsRefToMessageId)
+                .WithErrorResult(errorResult)
+                .Build();
+        }
+
+        private static Task<T> GetPMode<T>(string pmode) where T : class
+        {
+            return AS4XmlSerializer.FromStringAsync<T>(pmode);
+        }
+
+        protected virtual async Task<NotifyMessageEnvelope> CreateNotifyMessageEnvelope(AS4Message as4Message, Type receivedEntityType)
+        {
+            NotifyMessage notifyMessage = AS4MessageToNotifyMessageMapper.Convert(as4Message);
+
+            if (notifyMessage?.StatusInfo != null)
+            {
+                notifyMessage.StatusInfo.Status = Status.Exception;
+            }
+
+            string serialized = await AS4XmlSerializer.ToStringAsync(notifyMessage);
+
+            return new NotifyMessageEnvelope(
+                notifyMessage.MessageInfo,
+                notifyMessage.StatusInfo.Status,
+                Encoding.UTF8.GetBytes(serialized),
+                "application/xml",
+                receivedEntityType);
+        }
+    }
+}
