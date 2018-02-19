791a6c0571bbec17625eac1220b8e801566cc858
Stijn Moreels <stijn.moreels@codit.eu>
2018-02-19 07:30:51 +0100

    Expand the Clean Up Component Test
    By expanding the Component Test for the Clean Up Agent and asserting not
    only on OutMessages with referenced Reception Awareness entries, but
    also on InMessages, OutExceptions ans InExceptions. We can be sure that
    the Clean Up Agent is working as expected.

--- source/Eu.EDelivery.AS4.ComponentTests/Agents/CleanUpAgentsFacts.cs
+++ source/Eu.EDelivery.AS4.ComponentTests/Agents/CleanUpAgentsFacts.cs
@@ -40,31 +40,69 @@ namespace Eu.EDelivery.AS4.ComponentTests.Agents
             // Arrange: Insert a "retired" OutMessage with a referenced Reception Awareness.
             Config config = Config.Instance;
             config.Initialize("settings.xml");
-
-            string ebmsMessageId = Guid.NewGuid().ToString();
+            
+            DateTimeOffset overdueTime = DateTimeOffset.UtcNow.AddDays(-2);
+            string outReferenceId = Guid.NewGuid().ToString(), 
+                   outStandaloneId = Guid.NewGuid().ToString(),
+                   inMessageId = Guid.NewGuid().ToString(),
+                   outExceptionId = Guid.NewGuid().ToString(),
+                   inExceptionId = Guid.NewGuid().ToString();
 
             var databaseSpy = new DatabaseSpy(config);
-            databaseSpy.InsertOutMessage(
-                CreateOutMessage(ebmsMessageId, insertionTime: DateTimeOffset.UtcNow.AddDays(-2)));
+            databaseSpy.InsertOutMessage(CreateOutMessage(outReferenceId, insertionTime: overdueTime));
+            databaseSpy.InsertOutMessage(CreateOutMessage(outStandaloneId, insertionTime: overdueTime));
+            databaseSpy.InsertInMessage(CreateInMessage(inMessageId, overdueTime));
+            databaseSpy.InsertOutException(CreateOutException(outExceptionId, overdueTime));
+            databaseSpy.InsertInException(CreateInException(inExceptionId, overdueTime));
 
-            InsertReferencedReceptionAwareness(config, ebmsMessageId);
+            InsertReferencedReceptionAwareness(config, outReferenceId);
 
             // Act: AS4.NET Component will start the Clean Up Agent.
-            _msh = AS4Component.Start(Environment.CurrentDirectory);
+            ExerciseStartCleaning();
 
             // Assert: No OutMessage or Reception Awareness entries must be found for a given EbmsMessageId.
-            // Wait till AS4.NET Component has cleaned up the Messages Tables.
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-
-            Assert.Null(databaseSpy.GetOutMessageFor(m => m.EbmsMessageId.Equals(ebmsMessageId)));
-            Assert.Null(GetReferencedReceptionAwareness(config, ebmsMessageId));
+            Assert.Empty(databaseSpy.GetOutMessages(outReferenceId, outStandaloneId));
+            Assert.Empty(databaseSpy.GetInMessages(inMessageId));
+            Assert.Empty(databaseSpy.GetOutExceptions(outExceptionId));
+            Assert.Empty(databaseSpy.GetInExceptions(inExceptionId));
+            Assert.Null(GetReferencedReceptionAwareness(config, outReferenceId));
         }
 
         private static OutMessage CreateOutMessage(string ebmsMessageId, DateTimeOffset insertionTime)
         {
             return new OutMessage(ebmsMessageId: ebmsMessageId)
             {
-                MessageLocation = Registry.Instance.MessageBodyStore.SaveAS4Message(Config.Instance.InMessageStoreLocation, AS4Message.Empty),
+                MessageLocation = 
+                    Registry.Instance.MessageBodyStore.SaveAS4Message(
+                        Config.Instance.InMessageStoreLocation, 
+                        AS4Message.Empty),
+                InsertionTime = insertionTime
+            };
+        }
+
+        private static InMessage CreateInMessage(string ebmsMessageId, DateTimeOffset insertionTime)
+        {
+            return new InMessage(ebmsMessageId)
+            {
+                MessageLocation = Registry.Instance.MessageBodyStore.SaveAS4Message(
+                    Config.Instance.InMessageStoreLocation,
+                    AS4Message.Empty),
+                InsertionTime = insertionTime
+            };
+        }
+
+        private static OutException CreateOutException(string ebmsMessageId, DateTimeOffset insertionTime)
+        {
+            return new OutException(ebmsMessageId, errorMessage: string.Empty)
+            {
+                InsertionTime = insertionTime
+            };
+        }
+
+        private static InException CreateInException(string ebmsMessageId, DateTimeOffset insertionTime)
+        {
+            return new InException(ebmsMessageId, errorMessage: string.Empty)
+            {
                 InsertionTime = insertionTime
             };
         }
@@ -81,6 +119,14 @@ namespace Eu.EDelivery.AS4.ComponentTests.Agents
             }
         }
 
+        private void ExerciseStartCleaning()
+        {
+            _msh = AS4Component.Start(Environment.CurrentDirectory);
+
+            // Wait till AS4.NET Component has cleaned up the Messages Tables.
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
         private static ReceptionAwareness GetReferencedReceptionAwareness(IConfig config, string ebmsMessageId)
         {
             using (var ctx = new DatastoreContext(config))
@@ -95,4 +141,6 @@ namespace Eu.EDelivery.AS4.ComponentTests.Agents
         }
     }
 }
+
+
 
