2bb38661fa80f496a6de2410242fb2f9565f14b4
Stijn Moreels <stijn.moreels@codit.eu>
2018-03-06 14:37:44 +0100

    Add AllowUnknownRootCertificate option to the Receiving Processing Mode
    By updating the `SignatureVerification` model and its corresponding HTML file,
    we can add the `AllowUnknownRootCertificate` option to it.
    
     #8409

--- source/Fe/Eu.EDelivery.AS4.Fe/ui/src/app/api/SigningVerification.ts
+++ source/Fe/Eu.EDelivery.AS4.Fe/ui/src/app/api/SigningVerification.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 export class SigningVerification {
 	signature: number;
+	allowUnknownRootCertificate: boolean;
 
 	static FIELD_signature: string = 'signature';	
+	static FIELD_allowUnknownRootCertificate: string = 'allowUnknownRootCertificate';
 }
