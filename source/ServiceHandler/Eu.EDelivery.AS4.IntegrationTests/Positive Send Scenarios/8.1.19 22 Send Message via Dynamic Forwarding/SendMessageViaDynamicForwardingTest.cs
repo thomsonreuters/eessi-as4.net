08bb8c7122cce2e98ed3e75acefc37f21265ea3c
Frederik Gheysels <frederik.gheysels@codit.eu>
2018-03-20 12:10:04 +0100

    Dynamic Forwarding integration-tests: make sure that the same base p-mode is used for testcase 8.1.19; 8.1.20 and 8.1.22

--- source/ServiceHandler/Eu.EDelivery.AS4.IntegrationTests/Positive Send Scenarios/8.1.19 22 Send Message via Dynamic Forwarding/SendMessageViaDynamicForwardingTest.cs
+++ source/ServiceHandler/Eu.EDelivery.AS4.IntegrationTests/Positive Send Scenarios/8.1.19 22 Send Message via Dynamic Forwarding/SendMessageViaDynamicForwardingTest.cs
@@ -34,7 +34,7 @@ namespace Eu.EDelivery.AS4.IntegrationTests.Positive_Send_Scenarios._8._1._19_22
 
             InsertSmpConfigurationForAS4Component(url: ReceiveAgentEndpoint, enableEncryption: false);
 
-            AS4Message userMessage = UserMessageWithAttachment(argRefPModeId: "8.1.19-pmode");
+            AS4Message userMessage = UserMessageWithAttachment(argRefPModeId: "dynamic-forwarding-pmode");
 
             // Act: send the UserMessage to the AS4.NET Component, so it can be made complete by Dynamic Discover the info from the SMP Configuration
             SendMultiHopAS4Message(userMessage);
@@ -60,7 +60,7 @@ namespace Eu.EDelivery.AS4.IntegrationTests.Positive_Send_Scenarios._8._1._19_22
 
             InsertSmpConfigurationForAS4Component(url: ReceiveAgentEndpoint, enableEncryption: true);
 
-            AS4Message userMessage = UserMessageWithAttachment(argRefPModeId: "8.1.20-pmode");
+            AS4Message userMessage = UserMessageWithAttachment(argRefPModeId: "dynamic-forwarding-pmode");
 
             // Act: send the UserMessage to the AS4.NET Component, so it can be made complete by Dynamic Discover the info from the SMP Configuration
             SendMultiHopAS4Message(userMessage);
