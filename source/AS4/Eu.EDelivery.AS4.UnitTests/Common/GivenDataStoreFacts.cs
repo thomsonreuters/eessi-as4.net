fa1fea843cff4c669decfec628dbbf8f20328fc6
Stijn Moreels <stijn.moreels@codit.eu>
2018-04-03 07:53:11 +0200

    Ignore in memory warning

--- source/AS4/Eu.EDelivery.AS4.UnitTests/Common/GivenDataStoreFacts.cs
+++ source/AS4/Eu.EDelivery.AS4.UnitTests/Common/GivenDataStoreFacts.cs
@@ -38,14 +38,12 @@ namespace Eu.EDelivery.AS4.UnitTests.Common
         {
             // Create a new options instance telling the context to use an
             // InMemory database and the new service provider.
-            return
-                new DbContextOptionsBuilder<DatastoreContext>().UseInMemoryDatabase(Guid.NewGuid().ToString())
-                                                               .UseInternalServiceProvider(_serviceProvider)
-                                                               .ConfigureWarnings(
-                                                                   w =>
-                                                                       w.Ignore(
-                                                                           RelationalEventId.AmbientTransactionWarning))
-                                                               .Options;
+            return new DbContextOptionsBuilder<DatastoreContext>()
+                   .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                   .UseInternalServiceProvider(_serviceProvider)
+                   .ConfigureWarnings(w => w.Ignore(RelationalEventId.AmbientTransactionWarning))
+                   .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                   .Options;
         }
 
         /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
