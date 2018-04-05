5b0fb720fff1545b9e63e4f2fc0867452bf97eeb
Stijn Moreels <stijn.moreels@codit.eu>
2018-04-05 08:52:09 +0200

    Stop also Holodeck on Cancel

--- source/ServiceHandler/Eu.EDelivery.AS4.IntegrationTests/Scripts/stop-as4-component-application.ps1
+++ source/ServiceHandler/Eu.EDelivery.AS4.IntegrationTests/Scripts/stop-as4-component-application.ps1
@@ -1,1 +1,2 @@
 ﻿Stop-Process -Name Eu.EDelivery.AS4.ServiceHandler.ConsoleHost -Force -ErrorAction SilentlyContinue
+Stop-Process -Name "Java(TM) Platform SE binary" -Force -ErrorAction SilentlyContinue
