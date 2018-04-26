0ecaad250a1067069a7571f32323b5ebea6e251a
Stijn Moreels <stijn.moreels@codit.eu>
2018-04-26 13:24:21 +0200

    Remove server startup from 'reset.cmd'

--- source/Fe/Eu.EDelivery.AS4.Fe/ui/cypress/reset.cmd
+++ source/Fe/Eu.EDelivery.AS4.Fe/ui/cypress/reset.cmd
@@ -2,6 +2,5 @@ cd ..\..\..\..\output
 
 del .\database\users.sqlite
 copy .\samples\pmodes\03-send-pmode.xml .\config\send-pmodes\
-.\Eu.EDelivery.AS4.ServiceHandler.ConsoleHost.exe
 
 cd ..\source\Fe\Eu.EDelivery.AS4.Fe\ui
