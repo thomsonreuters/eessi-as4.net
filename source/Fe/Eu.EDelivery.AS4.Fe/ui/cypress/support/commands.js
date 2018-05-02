a88a21fbc5880eb2ad809e6c6b540cf9119835bf
Stijn Moreels <stijn.moreels@codit.eu>
2018-05-02 13:15:20 +0200

    Remove commented-code

--- source/Fe/Eu.EDelivery.AS4.Fe/ui/cypress/support/commands.js
+++ source/Fe/Eu.EDelivery.AS4.Fe/ui/cypress/support/commands.js
@@ -27,24 +27,14 @@
 Cypress.Commands.add('getdatacy', (s) => cy.get(`[data-cy=\"${s}\"]`))
 
 Cypress.Commands.add('login', () => {
-    // cy.visit('/login', { timeout: 20000 })
-    //     .then((_) => {
-    //         cy.fixture('login').then((json) => {
-    //             cy.getdatacy('username').type(json.username)
-    //             cy.getdatacy('password').type(json.password)
-    //             cy.getdatacy('login').click()
-    //             cy.url().should('contain', '/settings/portal')
-    //         })
-    //     })
-
     cy.fixture('login').then((json) => {
-        cy.request({ 
+        cy.request({
             method: 'POST',
             url: 'api/authentication',
             body: {
                 username: json.username,
                 password: json.password
-            } 
+            }
         }).then((resp) => {
             let token = resp.body.access_token;
             localStorage.setItem("id_token", token)
