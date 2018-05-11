dc937fb728ef0901f1d5768bc4850b8e0d1a3a0b
Stijn Moreels <stijn.moreels@codit.eu>
2018-05-11 11:04:58 +0200

    PR-FIX: return 500 HTTP StatusCode when the configured ReceivingPMode can't be found

---
+++ source/Eu.EDelivery.AS4.ComponentTests/Agents/StaticReceiveAgentFacts.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Eu.EDelivery.AS4.ComponentTests.Common;
+using Eu.EDelivery.AS4.Model.Core;
+using Eu.EDelivery.AS4.Model.Internal;
+using Eu.EDelivery.AS4.Serialization;
+using Eu.EDelivery.AS4.TestUtils.Stubs;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.ComponentTests.Agents
+{
+    public class StaticReceiveAgentFacts : ComponentTestTemplate
+    {
+        [Fact]
+        public async Task Agent_Returns_500_StatusCode_When_ReceivingPMode_Cannot_Be_Found()
+        {
+            // Arrange
+            const string settingsFileName = "staticreceiveagent_http_settings.xml";
+            OverrideTransformerReceivingPModeSetting(settingsFileName, pmodeId: "non-existing-pmode-id");
+
+            Settings receiveSettings = OverrideSettings(settingsFileName);
+            string url = receiveSettings.Agents
+                .ReceiveAgents.First().Receiver
+                .Setting.First(s => s.Key == "Url").Value;
+
+            var msh = AS4Component.Start(Environment.CurrentDirectory);
+
+            // Act
+            HttpResponseMessage response = await StubSender.SendAS4Message(url, AS4Message.Empty);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+            
+            // TearDown
+            msh.Dispose();
+        }
+
+        private void OverrideTransformerReceivingPModeSetting(string settingsFileName, string pmodeId)
+        {
+            string settingsFilePath = Path.Combine(ComponentTestSettingsPath, settingsFileName);
+            var settings = AS4XmlSerializer.FromString<Settings>(File.ReadAllText(settingsFilePath));
+
+            settings.Agents.ReceiveAgents.First()
+                    .Transformer
+                    .Setting.First(s => s.Key == "ReceivingPMode").Value = pmodeId;
+
+            File.WriteAllText(settingsFilePath, AS4XmlSerializer.ToString(settings));
+        }
+    }
+}
--- source/Transformers/Eu.EDelivery.AS4.Transformers/ReceiveMessageTransformer.cs
+++ source/Transformers/Eu.EDelivery.AS4.Transformers/ReceiveMessageTransformer.cs
@@ -87,10 +87,13 @@ namespace Eu.EDelivery.AS4.Transformers
                 }
                 else
                 {
-                    Logger.Warn(
-                        $"Receiving PMode with Id: {ReceivingPMode} was configured as default PMode, {Environment.NewLine}" +
-                        $"but this PMode cannot be found in the configured receiving PModes. {Environment.NewLine}" +
-                        @"Configured Receiving PModes are placed on the folder: '.\config\receive-pmodes\'.");
+                    string description =
+                        $"Receiving PMode with Id: {ReceivingPMode} was configured as default PMode, {Environment.NewLine}" + 
+                        "but this PMode cannot be found in the configured receiving PModes.";
+                    Logger.Error(
+                        $@"{description} Configured Receiving PModes are placed on the folder: '.\config\receive-pmodes\'.");
+
+                    throw new InvalidOperationException(description);
                 }
             }
 
