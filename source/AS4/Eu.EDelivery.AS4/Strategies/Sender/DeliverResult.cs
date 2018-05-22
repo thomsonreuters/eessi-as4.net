4157c04fc67d44464cdd3ae9b9897d6b2b0daaff
Stijn Moreels <stijn.moreels@codit.eu>
2018-05-22 06:47:32 +0200

    PR-SUG: move DeliveryStatus to seperate file

--- source/AS4/Eu.EDelivery.AS4/Strategies/Sender/DeliverResult.cs
+++ source/AS4/Eu.EDelivery.AS4/Strategies/Sender/DeliverResult.cs
@@ -46,30 +46,4 @@ namespace Eu.EDelivery.AS4.Strategies.Sender
             return other?.Status.Equals(Status) ?? false;
         }
     }
-
-    public enum DeliveryStatus
-    {
-        Success,
-        RetryableFail,
-        Fail
-    }
-
-    public static class DeliveryStatusEx
-    {
-        public static DeliveryStatus Reduce(DeliveryStatus x, DeliveryStatus y)
-        {
-            bool bothScuccess =
-                x == DeliveryStatus.Success
-                && y == DeliveryStatus.Success;
-
-            if (bothScuccess)
-            {
-                return DeliveryStatus.Success;
-            }
-
-            return x == DeliveryStatus.Fail || y == DeliveryStatus.Fail
-                ? DeliveryStatus.Fail
-                : DeliveryStatus.RetryableFail;
-        }
-    }
 }
---
+++ source/AS4/Eu.EDelivery.AS4/Strategies/Sender/DeliveryStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace Eu.EDelivery.AS4.Strategies.Sender
+{
+    public enum DeliveryStatus
+    {
+        Success,
+        RetryableFail,
+        Fail
+    }
+
+    public static class DeliveryStatusEx
+    {
+        public static DeliveryStatus Reduce(DeliveryStatus x, DeliveryStatus y)
+        {
+            bool bothScuccess =
+                x == DeliveryStatus.Success
+                && y == DeliveryStatus.Success;
+
+            if (bothScuccess)
+            {
+                return DeliveryStatus.Success;
+            }
+
+            return x == DeliveryStatus.Fail || y == DeliveryStatus.Fail
+                ? DeliveryStatus.Fail
+                : DeliveryStatus.RetryableFail;
+        }
+    }
+}
