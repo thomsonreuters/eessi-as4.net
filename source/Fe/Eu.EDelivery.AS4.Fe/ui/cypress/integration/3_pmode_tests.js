69addc62d64238ca90f9b1f4d97476e28a7f4b20
Stijn Moreels <stijn.moreels@codit.eu>
2018-06-04 08:53:01 +0200

    Adds UI test to verify the user defined values for the Retry Reliability inputs
    Verifies if we safe a pmode with user defined values other than the
    defaults, we still get the user defined values over the default ones.

--- source/Fe/Eu.EDelivery.AS4.Fe/ui/cypress/integration/3_pmode_tests.js
+++ source/Fe/Eu.EDelivery.AS4.Fe/ui/cypress/integration/3_pmode_tests.js
@@ -1,12 +1,16 @@
 describe('pmode tests', () => {
   beforeEach(() => cy.login());
 
-  const testDefaultRetryReliability = (x) => {
+  const withinTab = (x, f) => {
     cy
       .get('li > a[data-toggle=tab]:contains(' + x + ')')
       .click({ force: true });
 
-    cy.get('div[title="' + x + '"]').within(() => {
+    cy.get('div[title="' + x + '"]').within(f);
+  };
+
+  const testDefaultRetryReliability = (x) => {
+    withinTab(x, () => {
       cy.getdatacy('retry.count').should('to.be.disabled');
       cy.getdatacy('retry.interval').should('to.be.disabled');
 
@@ -31,6 +35,27 @@ describe('pmode tests', () => {
       cy.getdatacy('select-pmodes').select('02-sample-pmode', { force: true });
 
       testDefaultRetryReliability(x.handling);
+    });
+  });
+
+  it.only('should use the user-specified retry reliability values over the defaults', () => {
+    cy.visit('/pmodes/receiving');
+    cy.getdatacy('select-pmodes').select('01-sample-pmode', { force: true });
+
+    withinTab('Message handling', () => {
+      cy.getdatacy('retry.isEnabled').check({ force: true });
+      cy.getdatacy('retry.count').type('{selectall}5');
+      cy.getdatacy('retry.interval').type('{selectall}0:00:00:05');
+      cy.getdatacy('retry.isEnabled').uncheck({ force: true });
+    });
+
+    cy.getdatacy('save').click();
+    cy.reload();
+
+    withinTab('Message handling', () => {
+      cy.getdatacy('retry.isEnabled').check({ force: true });
+      cy.getdatacy('retry.count').should('to.have.value', '5');
+      cy.getdatacy('retry.interval').should('to.have.value', '0:00:00:05');
     });
   });
 });
