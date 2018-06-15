b1ab970125d2070cef1a53d24d87ff2f6d7d7ebe
Stijn Moreels <stijn.moreels@codit.eu>
2018-06-15 08:04:23 +0200

    Update the submit payload mapping

--- source/AS4/Eu.EDelivery.AS4/Mappings/Submit/SubmitMessageMap.cs
+++ source/AS4/Eu.EDelivery.AS4/Mappings/Submit/SubmitMessageMap.cs
@@ -23,7 +23,6 @@ namespace Eu.EDelivery.AS4.Mappings.Submit
                 .ForMember(dest => dest.Sender, src => src.ResolveUsing(SubmitSenderPartyResolver.Default.Resolve))
                 .ForMember(dest => dest.Receiver, src => src.ResolveUsing(SubmitReceiverResolver.Default.Resolve))
                 .ForMember(dest => dest.MessageProperties, src => src.ResolveUsing(SubmitMessagePropertiesResolver.Default.Resolve))
-                .ForMember(dest => dest.PayloadInfo, src => src.ResolveUsing(SubmitPayloadInfoResolver.Default.Resolve))
 
                 .ForMember(dest => dest.CollaborationInfo, src => src.MapFrom(s => s.Collaboration))
                 .ForMember(dest => dest.IsTest, src => src.Ignore())
@@ -40,6 +39,15 @@ namespace Eu.EDelivery.AS4.Mappings.Submit
                         userMessage.CollaborationInfo.Service = SubmitServiceResolver.Default.Resolve(submitMessage);
                         userMessage.CollaborationInfo.Action =  SubmitActionResolver.Default.Resolve(submitMessage);
                         userMessage.CollaborationInfo.ConversationId = SubmitConversationIdResolver.Default.Resolve(submitMessage);
+
+                        if (submitMessage.HasPayloads)
+                        {
+                            foreach (Model.Core.PartInfo p in 
+                                SubmitPayloadInfoResolver.Default.Resolve(submitMessage))
+                            {
+                                userMessage.AddPartInfo(p);
+                            }
+                        }
                     });
         }
     }
