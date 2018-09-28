a07497c20a01de87acc92a62efe029c096ed3f42
Stijn Moreels <stijn.moreels@codit.eu>
2018-09-28 07:36:25 +0200

    HOTFIX: run FE in IIS
    - Add `release.ps1` script for better debugging of releasing an AS4.NET package
    - Force x64 (64-bit) projects (both Debug and Release)
    - Force Assembly Redirects in FE project
    - Move the FE exe to the root of the package instead of the `.\bin\` folder, otherwise it can't be run standalone
    
     #9991
     #10036
     #9988

--- scripts/add-probing.ps1
+++ scripts/add-probing.ps1
@@ -1,13 +1,9 @@
-﻿$consoleHostConfig = "..\output\Eu.EDelivery.AS4.ServiceHandler.ConsoleHost.exe.config"
+﻿function add-probing ($config) {
+    $xml = [xml](Get-Content $config)
+    $xml.configuration.runtime.InnerXml = '<assemblyBinding xmlns="urn:schemas-microsoft-com:asm.v1"><probing privatePath="bin;"/></assemblyBinding>' + $xml.configuration.runtime.InnerXml
+    $xml.Save((Resolve-Path $config))
+}
 
-$consoleHostXml = [xml](Get-Content $consoleHostConfig)
-$consoleHostXml.configuration.runtime.InnerXml = '<assemblyBinding xmlns="urn:schemas-microsoft-com:asm.v1"><probing privatePath="bin;"/></assemblyBinding>' + $consoleHostXml.configuration.runtime.InnerXml
-
-$consoleHostXml.Save((Resolve-Path $consoleHostConfig))
-
-$windowsServiceConfig = "..\output\Eu.EDelivery.AS4.WindowsService.exe.config"
-
-$windowsServiceXml = [xml](Get-Content $windowsServiceConfig)
-$windowsServiceXml.configuration.runtime.InnerXml = '<assemblyBinding xmlns="urn:schemas-microsoft-com:asm.v1"><probing privatePath="bin;"/></assemblyBinding>' + $windowsServiceXml.configuration.runtime.InnerXml
-
-$windowsServiceXml.Save((Resolve-Path $windowsServiceConfig))
+add-probing "..\output\Eu.EDelivery.AS4.ServiceHandler.ConsoleHost.exe.config"
+add-probing "..\output\Eu.EDelivery.AS4.WindowsService.exe.config"
+add-probing "..\output\Eu.EDelivery.AS4.Fe.exe.config"
