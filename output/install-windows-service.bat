db5959d2ab0fb4bb7b110e398c181a472a14d5c6
Stijn Moreels <stijn.moreels@codit.eu>
2018-10-15 07:55:47 +0200

    FIX: don't do anything with the system-tray during install script

--- output/install-windows-service.bat
+++ output/install-windows-service.bat
@@ -11,11 +11,3 @@ if exist %WINDIR%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe (
 		%WINDIR%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe .\Eu.EDelivery.AS4.WindowsService.exe
 	)
 )
-
-if exist .\Eu.EDelivery.AS4.WindowsService.SystemTray.exe (
-	.\Eu.EDelivery.AS4.WindowsService.SystemTray.exe
-) else (
-	if exist ..\bin\Eu.EDelivery.AS4.WindowsService.SystemTray.exe (
-	..\bin\Eu.EDelivery.AS4.WindowsService.SystemTray.exe) 
-)
-
