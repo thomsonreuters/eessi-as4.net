7b7e424be36878a981f5c85c676a9e7405de7e13
Frederik Gheysels <frederik.gheysels@codit.eu>
2018-10-22 21:43:44 +0200

    Remove IPayloadRetrieverProvider property from IRegistry interface

--- source/Eu.EDelivery.AS4/Common/IRegistry.cs
+++ source/Eu.EDelivery.AS4/Common/IRegistry.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using Eu.EDelivery.AS4.Repositories;
-using Eu.EDelivery.AS4.Strategies.Retriever;
-using Eu.EDelivery.AS4.Strategies.Sender;
 
 namespace Eu.EDelivery.AS4.Common
 {
@@ -10,7 +8,6 @@ namespace Eu.EDelivery.AS4.Common
         bool IsInitialized { get; }
         Func<DatastoreContext> CreateDatastoreContext { get; }
         ICertificateRepository CertificateRepository { get; }
-        IPayloadRetrieverProvider PayloadRetrieverProvider { get; }
         MessageBodyStore MessageBodyStore { get; }
     }
 }
