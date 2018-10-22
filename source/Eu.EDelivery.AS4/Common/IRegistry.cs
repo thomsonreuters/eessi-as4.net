dd5583516f78a1a4157e8b0712fc020a482d7ec0
Frederik Gheysels <frederik.gheysels@codit.eu>
2018-10-22 16:34:31 +0200

    Removed IAttachmentUploaderProvider member from IRegistry interface definition

--- source/Eu.EDelivery.AS4/Common/IRegistry.cs
+++ source/Eu.EDelivery.AS4/Common/IRegistry.cs
@@ -2,7 +2,6 @@
 using Eu.EDelivery.AS4.Repositories;
 using Eu.EDelivery.AS4.Strategies.Retriever;
 using Eu.EDelivery.AS4.Strategies.Sender;
-using Eu.EDelivery.AS4.Strategies.Uploader;
 
 namespace Eu.EDelivery.AS4.Common
 {
@@ -10,7 +9,6 @@ namespace Eu.EDelivery.AS4.Common
     {
         bool IsInitialized { get; }
         Func<DatastoreContext> CreateDatastoreContext { get; }
-        IAttachmentUploaderProvider AttachmentUploader { get; }
         ICertificateRepository CertificateRepository { get; }
         INotifySenderProvider NotifySenderProvider { get; }
         IPayloadRetrieverProvider PayloadRetrieverProvider { get; }
