5639b303008c9c63dd8f24bd739a382c46633d56
Stijn Moreels <stijn.moreels@codit.eu>
2018-11-05 14:16:26 +0100

    PR-SUG: add msitran command to scripts in SC

---
+++ scripts/update-msi-installutil64.ps1
@@ -0,0 +1,1 @@
+msitran -a ./source/Eu.EDelivery.AS4.WindowsService.Installer/UpdateInstallUtil64.mst ./source/Eu.EDelivery.AS4.WindowsService.Installer/Release/Eu.EDelivery.AS4.WindowsService.Installer.msi
