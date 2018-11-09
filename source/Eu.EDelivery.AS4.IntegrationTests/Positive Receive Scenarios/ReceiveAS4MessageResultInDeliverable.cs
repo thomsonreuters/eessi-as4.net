a8ca9479c8b9e9054b0aee97f875f562af469eb8
Stijn Moreels <stijn.moreels@codit.eu>
2018-11-09 16:06:29 +0100

    TEST: refactor positive receive integration test scenarios to simpler approach
    All the integration test for positive receiving are not somewhat better
    organized and cleaned-up.
    The holodeck messages are now generated instead of hard-coded; meaning
    we have beter control at what we submit and have increased
    maintainability if any changes are required across tests.

---
+++ source/Eu.EDelivery.AS4.IntegrationTests/Positive Receive Scenarios/ReceiveAS4MessageResultInDeliverable.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using Eu.EDelivery.AS4.IntegrationTests.Common;
+using Eu.EDelivery.AS4.IntegrationTests.Positive_Receive_Scenarios._8._3._18_Receive_Single_Payload_with_HTTP_Deliver;
+using Xunit;
+using static Eu.EDelivery.AS4.IntegrationTests.Properties.Resources;
+
+namespace Eu.EDelivery.AS4.IntegrationTests.Positive_Receive_Scenarios
+{
+    public class ReceiveAS4MessageResultInDeliverable : IntegrationTestTemplate
+    {
+        [Fact]
+        public async Task Test_8_3_14_AS4Message_Test_Message_Gets_Responded_But_Not_Delivered()
+        {
+            // Arrange
+            AS4Component.Start();
+            Holodeck.CopyPModeToHolodeckA("8.3.14-pmode.xml");
+
+            // Act
+            Holodeck.PutMessageSinglePayloadToHolodeckA("8.3.14-pmode");
+
+            // Assert
+            await PollingService.PollUntilPresentAsync(Holodeck.HolodeckALocations.InputPath);
+
+            Holodeck.AssertReceiptOnHolodeckA();
+            Assert.Empty(Directory.GetFiles(AS4Component.FullInputPath, "*.xml"));
+        }
+
+        [Fact]
+        public async Task Test_8_3_15_AS4Message_Duplicate_Doesnt_Get_Delivered()
+        {
+            // Arrange
+            AS4Component.Start();
+            const string contentType = "multipart/related; boundary=\"=-M9awlqbs/xWAPxlvpSWrAg==\"; type=\"application/soap+xml\"; charset=\"utf-8\"";
+
+            // Act
+            await HttpClient.SendMessageAsync(duplicated_as4message, contentType);
+            CleanUpFiles(AS4Component.FullInputPath);
+
+            await HttpClient.SendMessageAsync(duplicated_as4message, contentType);
+            Assert.Empty(Directory.GetFiles(AS4Component.FullInputPath, "*.xml"));
+        }
+
+        [Fact]
+        public async Task Test_8_3_16_Pulled_AS4Message_Gets_Delivered()
+        {
+            // Arrange
+            AS4Component.OverrideSettings("8.3.16-settings.xml");
+            AS4Component.Start();
+
+            Holodeck.CopyPModeToHolodeckB("8.3.16-pmode.xml");
+
+            // Act
+            Holodeck.CopyMessageToHolodeckB("8.3.16-sample.mmd");
+
+            // Assert
+            await PollingService.PollUntilPresentAsync(
+                AS4Component.FullInputPath,
+                fs => fs.Count() == 2,
+                timeout: TimeSpan.FromSeconds(50));
+
+            var deliverDir = new DirectoryInfo(AS4Component.FullInputPath);
+            FileInfo[] deliverables = deliverDir.GetFiles();
+
+            Assert.Contains(deliverables, f => f.Extension == ".xml");
+            Assert.Contains(deliverables, f => f.Extension == ".jpg"
+                                               && f.Length == AS4Component.SubmitSinglePayloadImage.Length);
+        }
+
+        private static readonly HttpClient PayloadServiceClient = new HttpClient();
+
+        [Fact]
+        public async Task Test_8_3_17_AS4Message_Gets_Delivered_To_Payload_Service()
+        {
+            // Arrange
+            AS4Component.OverrideSettings("8.3.17-settings.xml");
+
+            Holodeck.CopyPModeToHolodeckA("8.3.17-pmode.xml");
+            Holodeck.CopyMessageToHolodeckA("8.3.17-sample.mmd");
+
+            // Act
+            AS4Component.Start();
+
+            // Assert
+            IEnumerable<FileInfo> deliverables =
+                await PollingService.PollUntilPresentAsync(
+                    AS4Component.FullInputPath,
+                    fs => fs.Any(f => f.Extension == ".xml"));
+
+            var doc = new XmlDocument();
+            doc.LoadXml(File.ReadAllText(deliverables.First().FullName));
+            XmlElement locationElement = doc["DeliverMessage"]?["Payloads"]?["Payload"]?["Location"];
+            Assert.True(locationElement != null, "No payload location found in delivered DeliverMessage");
+
+            HttpResponseMessage downloadResponse = await PayloadServiceClient.GetAsync(locationElement.InnerText);
+            Assert.Equal(HttpStatusCode.OK, downloadResponse.StatusCode);
+        }
+
+        [Fact]
+        public void Test_8_3_18_AS4Message_Gets_Delivered_To_Http_Target()
+        {
+            // Before
+            const string location = "http://localhost:4001/";
+            using (SpyHttpDeliverTarget deliverTarget = SpyHttpDeliverTarget.AtLocation(location))
+            {
+                // Arrange
+                Holodeck.CopyPModeToHolodeckA("8.3.18-pmode.xml");
+                Holodeck.CopyMessageToHolodeckA("8.3.18-sample.mmd");
+
+                // Act
+                AS4Component.Start();
+
+                // Assert
+                Assert.True(deliverTarget.IsCalled, "Receive Single Payload Deliver HTTP Integration Test failed");
+                string deliverContent = deliverTarget.DeliveredMessage;
+                Assert.False(String.IsNullOrEmpty(deliverContent), "Delivered Message was empty");
+
+                var doc = new XmlDocument();
+                doc.LoadXml(deliverContent);
+                XmlNode deliverMessageNode = doc.SelectSingleNode("//*[local-name()='DeliverMessage']");
+                Assert.NotNull(deliverMessageNode);
+            }
+        }
+    }
+}
---
+++ source/Eu.EDelivery.AS4.IntegrationTests/Positive Receive Scenarios/ReceiveAS4MessageResultInReceipt.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using Eu.EDelivery.AS4.IntegrationTests.Common;
+using Eu.EDelivery.AS4.IntegrationTests.Positive_Receive_Scenarios._8._3._18_Receive_Single_Payload_with_HTTP_Deliver;
+using Xunit;
+
+namespace Eu.EDelivery.AS4.IntegrationTests.Positive_Receive_Scenarios
+{
+    public class ReceiveAS4MessageResultInReceipt : IntegrationTestTemplate
+    {
+        [Fact]
+        public async Task Test_8_3_1_AS4Message_With_Single_Payload_Result_In_File_Deliver()
+        {
+            // Arrange
+            AS4Component.Start();
+            Holodeck.CopyPModeToHolodeckA("8.3.1-pmode.xml");
+
+            // Act
+            Holodeck.PutMessageSinglePayloadToHolodeckA("8.3.1-pmode");
+
+            // Assert
+            await PollingService.PollUntilPresentAsync(
+                AS4Component.FullInputPath,
+                fs => fs.Any(f => f.Extension == ".xml"));
+
+            Holodeck.AssertSinglePayloadOnHolodeckA();
+            Holodeck.AssertReceiptOnHolodeckA();
+        }
+
+        [Fact]
+        public async Task Test_8_3_2_AS4Message_With_Multiple_Payloads_Result_In_File_Deliver()
+        {
+            await TestAS4MessageWithMultiplePayloadsUsingPModeAsync("8.3.2-pmode");
+        }
+
+        [Fact]
+        public async Task Test_8_3_3_AS4Message_With_Multiple_Compressed_Payloads_Result_In_File_Deliver()
+        {
+            await TestAS4MessageWithMultiplePayloadsUsingPModeAsync("8.3.3-pmode");
+        }
+
+        [Fact]
+        public async Task Test_8_3_4_AS4Message_With_Multiple_Payloads_Signed_MessagingHeader_Result_In_File_Deliver()
+        {
+            await TestAS4MessageWithMultiplePayloadsUsingPModeAsync("8.3.4-pmode");
+        }
+
+        [Fact]
+        public async Task Test_8_3_5_AS4Message_With_Multiple_Encrypted_Payloads_Result_In_File_Deliver()
+        {
+            await TestAS4MessageWithMultiplePayloadsUsingPModeAsync("8.3.5-pmode");
+        }
+
+        [Fact]
+        public async Task Test_8_3_6_AS4Message_With_Multiple_Compressed_Payloads_Signed_MessagingHeader_Result_In_File_Deliver()
+        {
+            await TestAS4MessageWithMultiplePayloadsUsingPModeAsync("8.3.6-pmode");
+        }
+
+        [Fact]
+        public async Task Test_8_3_7_AS4Message_With_Multiple_Compressed_Encrypted_Payloads_Result_In_File_Deliver()
+        {
+            await TestAS4MessageWithMultiplePayloadsUsingPModeAsync("8.3.7-pmode");
+        }
+
+        [Fact]
+        public async Task Test_8_3_8_AS4Message_With_Multiple_Compressed_Encrypted_Payloads_Signed_MessagingHeader_Result_In_File_Deliver()
+        {
+            await TestAS4MessageWithMultiplePayloadsUsingPModeAsync("8.3.8-pmode");
+        }
+
+        [Fact]
+        public async Task Test_8_3_9_AS4Message_With_Multiple_Encrypted_Payloads_Signed_MessagingHeader_Result_In_File_Deliver()
+        {
+            await TestAS4MessageWithMultiplePayloadsUsingPModeAsync("8.3.9-pmode");
+        }
+
+        private async Task TestAS4MessageWithMultiplePayloadsUsingPModeAsync(string pmodeId)
+        {
+            // Arrange
+            AS4Component.Start();
+            Holodeck.CopyPModeToHolodeckA($"{pmodeId}.xml");
+
+            // Act
+            Holodeck.PutMessageTwoPayloadsToHolodeckA(pmodeId);
+
+            // Assert
+            await PollingService.PollUntilPresentAsync(
+                AS4Component.FullInputPath,
+                fs => fs.Count() == 3
+                      && fs.Count(f => f.Extension == ".jpg") == 1
+                      && fs.Count(f => f.Extension == ".xml") == 2);
+
+            Holodeck.AssertSinglePayloadOnHolodeckA();
+            Holodeck.AssertReceiptOnHolodeckA();
+        }
+
+        [Fact]
+        public async Task Test_8_3_10_AS4Message_Gets_Responded_With_Sync_Receipt()
+        {
+            // Arrange
+            AS4Component.Start();
+            Holodeck.CopyPModeToHolodeckA("8.3.10-pmode.xml");
+
+            // Act
+            Holodeck.CopyMessageToHolodeckA("8.3.10-sample.mmd");
+
+            // Assert
+            await PollingService.PollUntilPresentAsync(Holodeck.HolodeckALocations.InputPath);
+            Holodeck.AssertReceiptOnHolodeckA();
+        }
+
+        [Fact]
+        public async Task Test_8_3_11_AS4Message_Gets_Responded_With_Non_Repudiation_Receipt()
+        {
+            // Arrange
+            AS4Component.Start();
+            Holodeck.CopyPModeToHolodeckA("8.3.11-pmode.xml");
+
+            // Act
+            Holodeck.CopyMessageToHolodeckA("8.3.11-sample.mmd");
+
+            // Assert
+            await PollingService.PollUntilPresentAsync(Holodeck.HolodeckALocations.InputPath);
+            Holodeck.AssertReceiptOnHolodeckA();
+        }
+
+        [Fact]
+        public async Task Test_8_3_12_AS4Message_With_Single_Payload_Gets_Responded_With_Async_Receipt()
+        {
+            // Arrange
+            AS4Component.Start();
+            Holodeck.CopyPModeToHolodeckA("8.3.12-pmode.xml");
+
+            // Act
+            Holodeck.PutMessageSinglePayloadToHolodeckA("8.3.12-pmode");
+
+            // Assert
+            await PollingService.PollUntilPresentAsync(
+                Holodeck.HolodeckALocations.InputPath,
+                fs => fs.Any(f => f.Extension == ".xml"),
+                timeout: TimeSpan.FromSeconds(50));
+
+            Holodeck.AssertReceiptOnHolodeckA();
+        }
+
+        [Fact]
+        public async Task Test_8_3_13_AS4Message_With_Single_Payload_Gets_Responded_With_Signed_NonRepudiation_Receipt()
+        {
+            // Arrange
+            AS4Component.Start();
+            Holodeck.CopyPModeToHolodeckA("8.3.13-pmode.xml");
+
+            // Act
+            Holodeck.PutMessageSinglePayloadToHolodeckA("8.3.13-pmode");
+
+            // Assert
+            await PollingService.PollUntilPresentAsync(
+                Holodeck.HolodeckALocations.InputPath,
+                fs => fs.Any(f => f.Extension == ".xml"),
+                timeout: TimeSpan.FromSeconds(50));
+
+            Holodeck.AssertReceiptOnHolodeckA();
+        }
+    }
+}
