af273dc916e0dce7e9658174ef7b9c8fc0b6f54f
Stijn Moreels <stijn.moreels@codit.eu>
2018-11-16 09:01:42 +0100

    PR-FIX: place system tray installer to separate file

--- source/Eu.EDelivery.AS4.WindowsService.SystemTray/Program.cs
+++ source/Eu.EDelivery.AS4.WindowsService.SystemTray/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.ComponentModel;
-using System.Configuration.Install;
-using System.Diagnostics;
-using System.IO;
 using System.Windows.Forms;
 
 namespace Eu.EDelivery.AS4.WindowsService.SystemTray
@@ -19,36 +14,6 @@ namespace Eu.EDelivery.AS4.WindowsService.SystemTray
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SystemTrayForm());
-        }
-    }
-
-    [RunInstaller(true)]
-    public partial class SystemTrayInstaller : Installer
-    {
-        /// <summary>
-        /// Raises the <see cref="E:System.Configuration.Install.Installer.AfterInstall" /> event.
-        /// </summary>
-        /// <param name="savedState">An <see cref="T:System.Collections.IDictionary" /> that contains the state of the computer after all the installers contained in the <see cref="P:System.Configuration.Install.Installer.Installers" /> property have completed their installations. </param>
-        protected override void OnAfterInstall(IDictionary savedState)
-        {
-            Process.Start(Path.GetDirectoryName(this.Context.Parameters["AssemblyPath"]) + @"\Eu.EDelivery.AS4.WindowsService.SystemTray.exe");
-        }
-
-        /// <summary>
-        /// When overridden in a derived class, removes an installation.
-        /// </summary>
-        /// <param name="savedState">An <see cref="T:System.Collections.IDictionary" /> that contains the state of the computer after the installation was complete. </param>
-        /// <exception cref="T:System.ArgumentException">The saved-state <see cref="T:System.Collections.IDictionary" /> might have been corrupted. </exception>
-        /// <exception cref="T:System.Configuration.Install.InstallException">An exception occurred while uninstalling. This exception is ignored and the uninstall continues. However, the application might not be fully uninstalled after the uninstallation completes. </exception>
-        public override void Uninstall(IDictionary savedState)
-        {
-            foreach (Process p in Process.GetProcessesByName("Eu.EDelivery.AS4.WindowsService.SystemTray"))
-            {
-                if (!p.HasExited)
-                {
-                    p.Kill();
-                }
-            }
         }
     }
 }
