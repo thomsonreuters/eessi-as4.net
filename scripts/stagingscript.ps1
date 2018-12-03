694f8b3a8caade9dbd55796385cd793470629a27
Stijn Moreels <stijn.moreels@codit.eu>
2018-12-03 11:06:21 +0100

    Show errors to console

--- scripts/stagingscript.ps1
+++ scripts/stagingscript.ps1
@@ -7,10 +7,14 @@ function build-msi (
     [string] $projectPath, 
     [string] $devEnvPath)
 {
-    $parameters = "/Rebuild Release " + $solutionPath + " /Project " + $projectPath + " /ProjectConfig Release "
+    $parameters = "/Rebuild Release " + $solutionPath + " /Project " + $projectPath + " /ProjectConfig Release /Out errors.txt"
     "Process to start [$devEnvPath $parameters]"
     $process = [System.Diagnostics.Process]::Start($devEnvPath, $parameters)
     $process.WaitForExit()
+
+    if (Test-Path -Path errors.txt) {
+        Get-Content -Path errors.txt
+    }
 }
 
 build-msi "./source/AS4.sln" "./source/Eu.EDelivery.AS4.WindowsService.Installer/Eu.EDelivery.AS4.WindowsService.Installer.vdproj" "C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/Common7/IDE/devenv.exe"
